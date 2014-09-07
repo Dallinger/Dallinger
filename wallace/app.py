cf21d1377bf578ae43ed11580f6d62ad64f8c10f
Jordan Suchow <suchow@berkeley.edu>
2014-09-07 02:21:13 -0700

    Revert "Initialize experiment on Flask app launch"
    This reverts commit 7365b511cbe5ffdc92f6a9156aaead5c6f314141.

--- wallace/app.py
+++ wallace/app.py
@@ -5,7 +5,6 @@ import db
 app = Flask(__name__)
 
 session = db.init_db(drop_all=True)
-experiment = experiments.Demo2(session)
 
 
 @app.route('/')
@@ -15,6 +14,7 @@ def index():
 
 @app.route('/demo2')
 def start():
+    experiment = experiments.Demo2(session)
     experiment.add_and_trigger_sources()      # Add any sources
     process = experiment.process  # Step through the process
     for i in xrange(experiment.num_steps):
