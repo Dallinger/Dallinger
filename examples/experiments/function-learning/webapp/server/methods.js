423dccd1528283fd25ea600077e7cfe8d98f8243
Jordan Suchow <suchow@berkeley.edu>
2014-09-11 03:27:04 -0700

    Place Meteor.methods in server directory

---
+++ examples/experiments/function-learning/webapp/server/methods.js
@@ -0,0 +1,28 @@
+Meteor.methods({
+    createAgent: function () {
+        this.unblock();
+        return HTTP.post(wallaceUrl + "agents");
+    },
+    getPendingTransmissions: function (uuid) {
+        this.unblock();
+        url = wallaceUrl + "transmissions?destination_uuid=" + uuid;
+        return HTTP.get(url);
+    },
+    getInfo: function (uuid) {
+        this.unblock();
+        url = wallaceUrl + "information/" + uuid;
+        return HTTP.get(url);
+    },
+    createInfo: function (origin_uuid, contents) {
+        this.unblock();
+        url = wallaceUrl +
+              "information?origin_uuid=" + origin_uuid +
+              "&contents=" + contents;
+        return HTTP.post(url);
+    },
+    allAgents: function () {
+        this.unblock();
+        url = wallaceUrl + "agents";
+        return HTTP.get(url);
+    }
+});
