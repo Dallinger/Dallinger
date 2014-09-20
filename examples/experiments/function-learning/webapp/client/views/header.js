c92ff4c234914548608eda5b2cf180f086824ceb
Jordan Suchow <suchow@berkeley.edu>
2014-09-19 17:27:36 -0700

    Tweak header logic

--- examples/experiments/function-learning/webapp/client/views/header.js
+++ examples/experiments/function-learning/webapp/client/views/header.js
@@ -5,17 +5,16 @@ Template.header.isTestingPhase = function () {
 Template.header.thisTrial = function () {
     N = Session.get("N");
     trialIndex = bounds(Session.get("trialsCompleted"), 0, N);
-    if (trialIndex == -1)
-        return 0;
-    else if(trialIndex < N/2)
+    if(trialIndex < N/2)
         return trialIndex + 1;
     else
         return bounds(trialIndex + 1 - N/2, 0, N/2);
 };
 
 Template.header.numTrials = function () {
-    if (trialIndex == -1)
-        return 0;
+    trialIndex = bounds(Session.get("trialsCompleted"), 0, N);
+    if (trialIndex < 0)
+        return "<loading>";
     else
         return Session.get("N")/2;
 };
