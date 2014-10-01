25d4dc5f6fa4b310bfb7ff5b372e671432d2569b
Jordan Suchow <suchow@berkeley.edu>
2014-10-01 13:15:40 -0700

    Create minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wallace',
+      version='0.1',
+      description='A platform for experimental evolution',
+      url='http://github.com/suchow/Wallace',
+      author='Berkeley CoCoSci',
+      author_email='wallace@cocosci.berkeley.edu',
+      license='MIT',
+      packages=['wallace'],
+      zip_safe=False)
