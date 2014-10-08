9b677ace8d1d13abe08244f47b7698e0133a9a54
Jordan Suchow <suchow@berkeley.edu>
2014-10-08 01:10:45 -0700

    Use --app identifier to match Heroku conventions

--- wallace/command_line.py
+++ wallace/command_line.py
@@ -165,8 +165,12 @@ def deploy():
 @wallace.command()
 @click.option('--id', default=None, help='ID of the deployed experiment')
 def export(id):
+@wallace.command()
+@click.option('--app', default=None, help='ID of the deployed experiment')
+def export(app):
     """Export the data."""
     print_header()
+    id = str(app)
 
     log("Preparing to export the data...")
 
