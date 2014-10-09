905c4e8198829933cf6887bc46d1350c64bc497a
Jordan Suchow <suchow@berkeley.edu>
2014-10-09 14:09:34 -0700

    Revert "Don't automatically import recruiters"
    This reverts commit 6f4125d4d78e15c5a31490482ed82c04510dd2f5.

--- wallace/__init__.py
+++ wallace/__init__.py
@@ -1,3 +1,3 @@
-from . import models, information, agents, networks, processes
+from . import models, information, agents, networks, processes, recruiters
 
-__all__ = ['models', 'information', 'agents', 'networks', 'processes']
+__all__ = ['models', 'information', 'agents', 'networks', 'processes', 'recruiters']
