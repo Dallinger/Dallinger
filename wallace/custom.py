a8aac18648f2b6b1040d37d996b896d3e5ff5fd8
Jordan Suchow <suchow@berkeley.edu>
2014-11-11 16:46:58 -0800

    Remove unused files and templates
    Closes #65

--- wallace/custom.py
+++ wallace/custom.py
@@ -1,8 +1,6 @@
 # this file imports custom routes into the experiment server
 
-from flask import Blueprint, render_template, request, jsonify, Response, \
-    abort, current_app
-from jinja2 import TemplateNotFound
+from flask import Blueprint, request, jsonify, Response, abort
 
 from psiturk.psiturk_config import PsiturkConfig
 from psiturk.experiment_errors import ExperimentError
@@ -43,45 +41,6 @@ except ImportError:
 #  add them here
 ###########################################################
 
-
-# ----------------------------------------------
-# example custom route
-# ----------------------------------------------
-@custom_code.route('/my_custom_view')
-def my_custom_view():
-    # Print message to server.log for debugging
-    current_app.logger.info("Reached /my_custom_view")
-    try:
-        return render_template('custom.html')
-    except TemplateNotFound:
-        abort(404)
-
-
-# ----------------------------------------------
-# example using HTTP authentication
-# ----------------------------------------------
-@custom_code.route('/my_password_protected_route')
-@myauth.requires_auth
-def my_password_protected_route():
-    try:
-        return render_template('custom.html')
-    except TemplateNotFound:
-        abort(404)
-
-
-# ----------------------------------------------
-# example accessing data
-# ----------------------------------------------
-@custom_code.route('/view_data')
-@myauth.requires_auth
-def list_my_data():
-    users = Participant.query.all()
-    try:
-        return render_template('list.html', participants=users)
-    except TemplateNotFound:
-        abort(404)
-
-
 # ----------------------------------------------
 # example computing bonus
 # ----------------------------------------------
@@ -120,12 +79,13 @@ def compute_bonus():
 def launch():
     exp = experiment(db_session_w)
 
-    try:
-        # Submit the first HIT
-        exp.recruiter().open_recruitment(exp)
-        return render_template('launch.html')
-    except TemplateNotFound:
-        abort(404)
+    # Submit the first HIT
+    exp.recruiter().open_recruitment(exp)
+
+    # Return a response.
+    data = {"status": "launched"}
+    js = dumps(data)
+    return Response(js, status=200, mimetype='application/json')
 
 
 @custom_code.route("/agents", methods=["POST", "GET"])
