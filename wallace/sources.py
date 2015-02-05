c0d3d239ee2ced0ccc4033589b5cde387c268d44
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-02-04 16:47:10 -0800

    Remove redundant stuff!

--- wallace/sources.py
+++ wallace/sources.py
@@ -16,25 +16,6 @@ class Source(Node):
         """Generate new information."""
         return NotImplementedError
 
-    def transmit(self, other_node, selector=None):
-
-        if selector is None:
-            cls = Info
-
-        elif issubclass(selector, Info):
-            cls = selector
-
-        info = cls(
-            origin=self,
-            origin_uuid=self.uuid,
-            contents=self._data())
-
-        super(Source, self).transmit(other_node, selector=info)
-
-    def broadcast(self):
-        for vector in self.outgoing_vectors:
-            self.transmit(vector.destination, selector=Info)
-
 
 class RandomBinaryStringSource(Source):
     """An agent whose genome and memome are random binary strings. The source
