eae85f5fc516f231a6d1a12de83c6368c86c7e8a
Jordan Suchow <suchow@berkeley.edu>
2015-02-27 01:36:16 -0800

    Tweak the function learning demo

--- examples/function-learning/experiment.py
+++ examples/function-learning/experiment.py
@@ -1,37 +1,29 @@
-from wallace.networks import Chain
-from wallace.processes import RandomWalkFromSource
-from wallace.recruiters import PsiTurkRecruiter
-from wallace.agents import ReplicatorAgent
-from wallace.experiments import Experiment
-from wallace.sources import Source
-from wallace.models import Network, Agent, Info
+import wallace
 import random
 import json
 from sqlalchemy.ext.declarative import declared_attr
 import math
 
 
-class FunctionLearning(Experiment):
+class FunctionLearning(wallace.experiments.Experiment):
     def __init__(self, session):
-        super(FunctionLearning, self).__init__(session)
-
-        self.task = "Transmission chain"
-        self.num_agents = 10
+        self.num_agents_per_chain = 10
         self.num_repeats = 4
-        self.num_steps = self.num_agents - 1
-        self.agent_type_generator = ReplicatorAgent
-        self.network_type = Chain
-        self.process_type = RandomWalkFromSource
-        self.recruiter = PsiTurkRecruiter
+        self.agent_type_generator = wallace.agents.ReplicatorAgent
+        self.network_type = wallace.networks.Chain
+        self.process_type = wallace.processes.RandomWalkFromSource
+        self.recruiter = wallace.recruiters.PsiTurkRecruiter
+
+        super(FunctionLearning, self).__init__(session)
 
         # Get a list of all the networks, creating them if they don't already
         # exist.
-        self.networks = Network.query.all()
+        self.networks = wallace.models.Network.query.all()
         if not self.networks:
             for i in range(self.num_repeats):
                 net = self.network_type()
                 self.session.add(net)
-        self.networks = Network.query.all()
+        self.networks = wallace.models.Network.query.all()
 
         # Setup for first time experiment is accessed
         for net in self.networks:
@@ -58,14 +50,16 @@ class FunctionLearning(Experiment):
             self.recruiter().recruit_new_participants(self, n=1)
 
     def is_experiment_over(self):
-        return len(Agent.query.all()) == self.num_agents
+        current = len(wallace.models.Agent.query.all())
+        needed = self.num_agents_per_chain * self.num_repeats
+        return current == needed
 
 
-class AbstractFunctionSource(Source):
+class AbstractFnSource(wallace.models.Source):
     __abstract__ = True
 
     def create_information(self):
-        info = Info(
+        info = wallace.models.Info(
             origin=self,
             origin_uuid=self.uuid,
             contents=self._data())
@@ -90,53 +84,53 @@ class AbstractFunctionSource(Source):
         return {"polymorphic_identity": cls.__name__.lower()}
 
 
-class IdentityFunctionSource(AbstractFunctionSource, Source):
+class IdentityFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return x
 
 
-class AdditiveInverseFunctionSource(AbstractFunctionSource, Source):
+class AdditiveInverseFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 100 - x
 
 
-class SinusoidalFunctionSource(AbstractFunctionSource, Source):
+class SinusoidalFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 50.5 + 49.5 * math.sin(math.pi / 2 + x / (5 * math.pi))
 
 
-class RandomMappingFunctionSource(AbstractFunctionSource, Source):
+class RandomMappingFunctionSource(AbstractFnSource, wallace.models.Source):
     m = random.shuffle(range(1, 100))
 
     def func(self, x):
         return self.m[x - 1]
 
 
-class StepFunctionSource(AbstractFunctionSource, Source):
+class StepFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 75 if x >= 50 else 25
 
 
-class ConstantFunctionSource(AbstractFunctionSource, Source):
+class ConstantFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 50
 
 
-class LogisticFunctionSource(AbstractFunctionSource, Source):
+class LogisticFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 1 / (0.01 + math.exp(-0.092 * x))
 
 
-class ExponentialFunctionSource(AbstractFunctionSource, Source):
+class ExponentialFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 100 * math.exp(-0.05 * x)
 
 
-class TriangleWaveFunctionSource(AbstractFunctionSource, Source):
+class TriangleWaveFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 2 * (100 - x) if x >= 50 else 2 * x
 
 
-class SquareWaveFunctionSource(AbstractFunctionSource, Source):
+class SquareWaveFunctionSource(AbstractFnSource, wallace.models.Source):
     def func(self, x):
         return 75 if (math.fmod(x, 50) <= 25) else 25
