deb3c2bd9cc117768adf7b97f377453fab87287a
Jordan Suchow <suchow@berkeley.edu>
2015-04-24 18:51:02 -0700

    Separate tests for command line from examples

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -16,20 +16,6 @@ class TestCommandLine(object):
         self.db.add_all(args)
         self.db.commit()
 
-    def verify_example(self, example):
-        os.chdir(example)
-        output = subprocess.check_output("wallace verify", shell=True)
-        assert "✗" not in output
-        os.chdir("..")
-
-    def test_verify_example_bartlett1932(self):
-        self.verify_example("bartlett1932")
-
-    def test_verify_example_function_learning(self):
-        self.verify_example("function-learning")
-
-    def test_verify_example_moran_process(self):
-        self.verify_example("moran-process")
-
-    def test_verify_example_translations(self):
-        self.verify_example("translations")
+    def test_wallace_help(self):
+        output = subprocess.check_output("wallace", shell=True)
+        assert("Usage: wallace [OPTIONS] COMMAND [ARGS]" in output)
---
+++ tests/test_examples.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+
+
+class TestExamples(object):
+
+    def setup(self):
+        os.chdir("examples")
+
+    def teardown(self):
+        os.chdir("..")
+
+    def add(self, *args):
+        self.db.add_all(args)
+        self.db.commit()
+
+    def verify_example(self, example):
+        os.chdir(example)
+        output = subprocess.check_output("wallace verify", shell=True)
+        assert "✗" not in output
+        os.chdir("..")
+
+    def test_verify_example_bartlett1932(self):
+        self.verify_example("bartlett1932")
+
+    def test_verify_example_function_learning(self):
+        self.verify_example("function-learning")
+
+    def test_verify_example_moran_process(self):
+        self.verify_example("moran-process")
+
+    def test_verify_example_translations(self):
+        self.verify_example("translations")
