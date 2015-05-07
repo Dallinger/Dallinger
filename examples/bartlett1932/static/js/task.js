750924e4c4b6d5d071d3f42b95f711444402232a
Jordan Suchow <suchow@berkeley.edu>
2015-05-06 17:39:41 -0700

    Wait a bit before requesting story

--- examples/bartlett1932/static/js/task.js
+++ examples/bartlett1932/static/js/task.js
@@ -13,15 +13,15 @@ var mycounterbalance = counterbalance;  // they tell you which condition you hav
 
 // All pages to be loaded
 var pages = [
-	"instructions/instruct-ready.html",
-	"stage.html",
-	"postquestionnaire.html"
+    "instructions/instruct-ready.html",
+    "stage.html",
+    "postquestionnaire.html"
 ];
 
 psiTurk.preloadPages(pages);
 
 var instructionPages = [ // add as a list as many pages as you like
-	"instructions/instruct-ready.html"
+    "instructions/instruct-ready.html"
 ];
 
 
@@ -40,96 +40,98 @@ var instructionPages = [ // add as a list as many pages as you like
 ********************/
 var StroopExperiment = function() {
 
-	// Load the stage.html snippet into the body of the page
-	psiTurk.showPage('stage.html');
-	$("#response-form").hide();
-	$("#finish-reading").hide();
+    // Load the stage.html snippet into the body of the page
+    psiTurk.showPage('stage.html');
+    $("#response-form").hide();
+    $("#finish-reading").hide();
 
-	// Create the agent.
-	createAgent = function() {
-		reqwest({
-		    url: "/agents",
-		    method: 'post',
-		    data: { unique_id: uniqueId },
-		    type: 'json',
-		  	success: function (resp) {
-		  		agent_uuid = resp.agents.uuid;
-		     	getPendingTransmissions(agent_uuid);
-		    },
-		    error: function (err) {
-			  	currentview = new Questionnaire();
-		    }
-		});
-	};
+    // Create the agent.
+    createAgent = function() {
+        reqwest({
+            url: "/agents",
+            method: 'post',
+            data: { unique_id: uniqueId },
+            type: 'json',
+            success: function (resp) {
+                agent_uuid = resp.agents.uuid;
+                setTimeout(function() {
+                    getPendingTransmissions(agent_uuid);
+                }, 2000);
+            },
+            error: function (err) {
+                currentview = new Questionnaire();
+            }
+        });
+    };
 
-	getPendingTransmissions = function(destination_uuid) {
-		reqwest({
-		    url: "/transmissions?destination_uuid=" + destination_uuid,
-		    method: 'get',
-		    type: 'json',
-		  	success: function (resp) {
-		  		info_uuid = resp.transmissions[0].info_uuid;
-		     	info = getInfo(info_uuid);
-		    },
-		    error: function (err) {
-		    	console.log(err);
-		    }
-		});
-	};
+    getPendingTransmissions = function(destination_uuid) {
+        reqwest({
+            url: "/transmissions?destination_uuid=" + destination_uuid,
+            method: 'get',
+            type: 'json',
+            success: function (resp) {
+                info_uuid = resp.transmissions[0].info_uuid;
+                info = getInfo(info_uuid);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    };
 
-	getInfo = function(uuid) {
-		reqwest({
-		    url: "/information/" + uuid,
-		    method: 'get',
-		    type: 'json',
-		  	success: function (resp) {
-		     	story = resp.contents;
-		     	storyHTML = markdown.toHTML(story);
-		     	$("#story").html(storyHTML);
-		     	$("#stimulus").show();
-				$("#response-form").hide();
-		     	$("#finish-reading").show();
-		    },
-		    error: function (err) {
-		    	console.log(err);
-		    }
-		});
-	};
+    getInfo = function(uuid) {
+        reqwest({
+            url: "/information/" + uuid,
+            method: 'get',
+            type: 'json',
+            success: function (resp) {
+                story = resp.contents;
+                storyHTML = markdown.toHTML(story);
+                $("#story").html(storyHTML);
+                $("#stimulus").show();
+                $("#response-form").hide();
+                $("#finish-reading").show();
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    };
 
-	createAgent();
+    createAgent();
 
-	$("#finish-reading").click(function() {
-		$("#stimulus").hide();
-		$("#response-form").show();
-		$("#submit-response").removeClass('disabled');
-		$("#submit-response").html('Submit');
-	});
+    $("#finish-reading").click(function() {
+        $("#stimulus").hide();
+        $("#response-form").show();
+        $("#submit-response").removeClass('disabled');
+        $("#submit-response").html('Submit');
+    });
 
-	$("#submit-response").click(function() {
+    $("#submit-response").click(function() {
 
-		$("#submit-response").addClass('disabled');
-		$("#submit-response").html('Sending...');
+        $("#submit-response").addClass('disabled');
+        $("#submit-response").html('Sending...');
 
-		response = encodeURIComponent($("#reproduction").val());
+        response = encodeURIComponent($("#reproduction").val());
 
-		$("#reproduction").val("");
+        $("#reproduction").val("");
 
-		reqwest({
-		    url: "/information",
-		  	method: 'post',
-		  	data: {
-		  		origin_uuid: agent_uuid,
-		  		contents: response,
-		  		info_type: "base"
-		  	},
-		  	success: function (resp) {
-		  		createAgent();
-		  	}
-		});
+        reqwest({
+            url: "/information",
+            method: 'post',
+            data: {
+                origin_uuid: agent_uuid,
+                contents: response,
+                info_type: "base"
+            },
+            success: function (resp) {
+                createAgent();
+            }
+        });
 
-		// currentview = new Questionnaire();
-		// psiTurk.recordTrialData({'phase':"TEST", 'response': $("#reproduction").val()});
-	});
+        // currentview = new Questionnaire();
+        // psiTurk.recordTrialData({'phase':"TEST", 'response': $("#reproduction").val()});
+    });
 
 };
 
@@ -139,55 +141,55 @@ var StroopExperiment = function() {
 
 var Questionnaire = function() {
 
-	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
+    var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-	record_responses = function() {
+    record_responses = function() {
 
-		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
+        psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
 
-		$('textarea').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);
-		});
-		$('select').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);
-		});
+        $('textarea').each( function(i, val) {
+            psiTurk.recordUnstructuredData(this.id, this.value);
+        });
+        $('select').each( function(i, val) {
+            psiTurk.recordUnstructuredData(this.id, this.value);
+        });
 
-	};
+    };
 
-	prompt_resubmit = function() {
-		replaceBody(error_message);
-		$("#resubmit").click(resubmit);
-	};
+    prompt_resubmit = function() {
+        replaceBody(error_message);
+        $("#resubmit").click(resubmit);
+    };
 
-	resubmit = function() {
-		replaceBody("<h1>Trying to resubmit...</h1>");
-		reprompt = setTimeout(prompt_resubmit, 10000);
+    resubmit = function() {
+        replaceBody("<h1>Trying to resubmit...</h1>");
+        reprompt = setTimeout(prompt_resubmit, 10000);
 
-		psiTurk.saveData({
-			success: function() {
-			    clearInterval(reprompt);
+        psiTurk.saveData({
+            success: function() {
+                clearInterval(reprompt);
                 psiTurk.computeBonus('compute_bonus', function(){finish()});
-			},
-			error: prompt_resubmit
-		});
-	};
+            },
+            error: prompt_resubmit
+        });
+    };
 
-	// Load the questionnaire snippet
-	psiTurk.showPage('postquestionnaire.html');
-	psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
+    // Load the questionnaire snippet
+    psiTurk.showPage('postquestionnaire.html');
+    psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
 
-	$("#next").click(function () {
-		$('#next').prop('disabled', true);
-		$("#next-symbol").attr('class', 'glyphicon glyphicon-refresh glyphicon-refresh-animate');
-	    record_responses();
-	    psiTurk.saveData({
+    $("#next").click(function () {
+        $('#next').prop('disabled', true);
+        $("#next-symbol").attr('class', 'glyphicon glyphicon-refresh glyphicon-refresh-animate');
+        record_responses();
+        psiTurk.saveData({
             success: function(){
                 psiTurk.computeBonus('compute_bonus', function() {
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                    psiTurk.completeHIT(); // when finished saving compute bonus, the quit
                 });
             },
             error: prompt_resubmit});
-	});
+    });
 
 
 };
@@ -200,7 +202,7 @@ var currentview;
  ******************/
 $(window).load( function(){
     psiTurk.doInstructions(
-    	instructionPages, // a list of pages you want to display in sequence
-    	function() { currentview = new StroopExperiment(); } // what you want to do when you are done with instructions
+        instructionPages, // a list of pages you want to display in sequence
+        function() { currentview = new StroopExperiment(); } // what you want to do when you are done with instructions
     );
 });
