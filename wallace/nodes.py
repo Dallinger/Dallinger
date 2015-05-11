68aca3d2e545d762f7454bc6fae530b2fa4d8f1f
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-05-11 12:42:04 -0700

    Environment: remove table, and remove repr

--- wallace/nodes.py
+++ wallace/nodes.py
@@ -92,12 +92,10 @@ class Environment(Node):
 
     """
     Environments are Nodes with the following features:
-    1 - they have the get_observed function
-    2 - by default they transmit the most recent info of type State
-    3 - the state() method can be passed a time to get the state at that time
+    1 - by default they transmit the most recent info of type State
+    2 - the state() method can be passed a time to get the state at that time
     """
 
-    __tablename__ = "environment"
     __mapper_args__ = {"polymorphic_identity": "environment"}
 
     # the unique environment id
@@ -118,7 +116,3 @@ class Environment(Node):
 
     def _what(self):
         return self.state()
-
-    def __repr__(self):
-        """Print the environment in a nice format."""
-        return "Environment-{}-{}".format(self.uuid[:6], self.type)
