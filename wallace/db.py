58271242c4557d121a3c0b993ac7b28be9bc0cb3
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-05-23 08:02:57 -0700

    change db_url

--- wallace/db.py
+++ wallace/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-db_url = os.environ.get("DATABASE_URL", "sqlite:///wallace.db")
+db_url = db_url = os.environ.get("DATABASE_URL", "postgresql://postgres@localhost/wallace")
 engine = create_engine(db_url)
 Session = scoped_session(sessionmaker(autoflush=True, bind=engine))
 
