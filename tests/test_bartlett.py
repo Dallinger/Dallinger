777d16349a6a55c92caf2182ca7f7bc9837f8bd7
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-07-14 16:21:34 -0700

    bartlett tests: create first worker

--- tests/test_bartlett.py
+++ tests/test_bartlett.py
@@ -16,19 +16,26 @@ class TestBartlett(object):
 
     args = {'hitId': 'bartlett-test-hit', 'assignmentId': 1, 'workerId': 1, 'mode': 'sandbox'}
     participant = requests.get(exp_address + '/exp', params=args)
-    print participant.status_code
     #print participant.text
 
     args = {'Event.1.EventType': 'AssignmentAccepted', 'Event.1.AssignmentId': 1}
     notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    notification = requests.post(exp_address + '/notifications', data=args)
-    print notification.status_code
-    #print notification.text
+
+    working = True
+    while working is True:
+        args = {'unique_id': '1:1'}
+        agent = requests.post(exp_address + '/agents', data=args)
+        print agent
+        print agent.contents
+        print agent.text
+        working = agent.status == 200
+        if working is True:
+            agent_uuid = agent.contents.agents.uuid
+            args = {'destination_uuid': agent_uuid}
+            transmission = requests.get(exp_address + '/transmissions', data=args)
+            info = requests.get(exp_address + '/info/' + transmission.contents.transmissions[0].info_uuid, data=args)
+            args = {'origin_uuid': agent_uuid, 'contents': 'test test test', 'info_type': 'base'}
+            requests.post(exp_address + '/information', data=args)
 
     #subprocess.call("heroku apps:destroy --app " + exp_id + " --confirm " + exp_id, shell=True)
     # for app in $(heroku apps | sed '/[pt]/ d'); do heroku apps:destroy --app $app --confirm $app; done
