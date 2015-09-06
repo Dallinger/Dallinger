264b4112ccfdebeb7524036b6f32d49fa38bb321
Jordan Suchow <suchow@post.harvard.edu>
2015-09-06 01:02:01 -0700

    Create test for sandboxing via Heroku

---
+++ tests/test_heroku.py
@@ -0,0 +1,22 @@
+"""Tests for the Wallace API."""
+
+import subprocess
+import re
+import requests
+
+
+class TestHeroku(object):
+
+    """The Heroku test class."""
+
+    def test_sandbox(self):
+        """Launch the experiment on Heroku."""
+        sandbox_output = subprocess.check_output(
+            "cd examples/bartlett1932; wallace sandbox --verbose", shell=True)
+
+        id = re.search(
+            'Running as experiment (.*)...', sandbox_output).group(1)
+
+        r = requests.get("http://{}.herokuapp.com/summary".format(id))
+
+        assert r.json()['status'] == []
