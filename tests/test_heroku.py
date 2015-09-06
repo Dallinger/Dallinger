12a9ef54d82d9508852e5596dbb9df321986e067
Jordan Suchow <suchow@post.harvard.edu>
2015-09-06 02:57:53 -0700

    Refactor Heroku tests to have shared setup

--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import re
+import os
 import requests
 
 
@@ -9,18 +10,22 @@ class TestHeroku(object):
 
     """The Heroku test class."""
 
-    def test_sandbox(self):
-        """Launch the experiment on Heroku."""
-        sandbox_output = subprocess.check_output(
-            "cd examples/bartlett1932; wallace sandbox --verbose", shell=True)
+    sandbox_output = subprocess.check_output(
+        "cd examples/bartlett1932; wallace sandbox --verbose", shell=True)
 
-        id = re.search(
-            'Running as experiment (.*)...', sandbox_output).group(1)
+    os.environ['app_id'] = re.search(
+        'Running as experiment (.*)...', sandbox_output).group(1)
 
-        r = requests.get("http://{}.herokuapp.com/summary".format(id))
-
-        assert r.json()['status'] == []
-
+    @classmethod
+    def teardown_class(cls):
+        """Remove the app from Heroku."""
+        app_id = os.environ['app_id']
         subprocess.call(
-            "heroku apps:destroy --app {} --confirm {}".format(id),
+            "heroku apps:destroy --app {} --confirm {}".format(app_id, app_id),
             shell=True)
+
+    def test_summary(self):
+        """Launch the experiment on Heroku."""
+        app_id = os.environ['app_id']
+        r = requests.get("http://{}.herokuapp.com/summary".format(app_id))
+        assert r.json()['status'] == []
