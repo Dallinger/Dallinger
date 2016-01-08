23d1317f5077cb6d4ed6ca519645addcbd3ce462
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-01-08 13:40:53 -0800

    add postmark to app

--- wallace/command_line.py
+++ wallace/command_line.py
@@ -301,6 +301,8 @@ def deploy_sandbox_shared_setup(verbose=True, web_procs=1):
 
         "heroku addons:create papertrail",
 
+        "heroku addons:create postmark:10k",
+
         "heroku config:set HOST=" +
         id + ".herokuapp.com",
 
