459dee20662d972ef6e089ef1244d300bb500269
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-01-08 10:34:17 -0800

    on second thought, actually do import the experiment

--- wallace/heroku/clock.py
+++ wallace/heroku/clock.py
@@ -2,7 +2,22 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from wallace import db, models
+import imp
+import inspect
 from psiturk.models import Participant
+
+# Specify the experiment.
+try:
+    exp = imp.load_source('experiment', "wallace_experiment.py")
+    classes = inspect.getmembers(exp, inspect.isclass)
+    exps = [c for c in classes
+            if (c[1].__bases__[0].__name__ in "Experiment")]
+    this_experiment = exps[0][0]
+    mod = __import__('wallace_experiment', fromlist=[this_experiment])
+    experiment = getattr(mod, this_experiment)
+except ImportError:
+    print "Error: Could not import experiment."
+
 session = db.session
 
 scheduler = BlockingScheduler()
