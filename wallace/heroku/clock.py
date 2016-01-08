592a613540560e428da5a43aab9f07d085b93779
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-01-08 10:11:46 -0800

    only import experiment once, try accessing db

--- wallace/heroku/clock.py
+++ wallace/heroku/clock.py
@@ -2,28 +2,35 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from wallace import db, models
+from psiturk.models import Participant
 import imp
 import inspect
 session = db.session
 
 scheduler = BlockingScheduler()
 
+try:
+    exp = imp.load_source('experiment', "wallace_experiment.py")
+    classes = inspect.getmembers(exp, inspect.isclass)
+    exps = [c for c in classes
+            if (c[1].__bases__[0].__name__ in "Experiment")]
+    this_experiment = exps[0][0]
+    mod = __import__('wallace_experiment', fromlist=[this_experiment])
+    experiment = getattr(mod, this_experiment)
+except ImportError:
+    print "Error: Could not import experiment."
+
+exp = experiment(session)
+
 
 @scheduler.scheduled_job('interval', minutes=0.25)
 def check_db_for_missing_notifications():
     print("1")
-    try:
-        exp = imp.load_source('experiment', "wallace_experiment.py")
-        classes = inspect.getmembers(exp, inspect.isclass)
-        exps = [c for c in classes
-                if (c[1].__bases__[0].__name__ in "Experiment")]
-        this_experiment = exps[0][0]
-        mod = __import__('wallace_experiment', fromlist=[this_experiment])
-        experiment = getattr(mod, this_experiment)
-    except ImportError:
-        print "Error: Could not import experiment."
+    participants = Participant.query.all()
+    print participants
     print("2")
-    exp = experiment(session)
+    nodes = models.Node.query.all()
+    print nodes
     print("3")
 
 scheduler.start()
