d31e4a6e38cb72760aa365603838a3afe65e3da4
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-01-08 10:19:20 -0800

    don't both importing the experiment

--- wallace/heroku/clock.py
+++ wallace/heroku/clock.py
@@ -3,28 +3,28 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from wallace import db, models
 from psiturk.models import Participant
-import imp
-import inspect
+# import imp
+# import inspect
 session = db.session
 
 scheduler = BlockingScheduler()
 
-try:
-    exp = imp.load_source('experiment', "wallace_experiment.py")
-    classes = inspect.getmembers(exp, inspect.isclass)
-    exps = [c for c in classes
-            if (c[1].__bases__[0].__name__ in "Experiment")]
-    this_experiment = exps[0][0]
-    mod = __import__('wallace_experiment', fromlist=[this_experiment])
-    experiment = getattr(mod, this_experiment)
-except ImportError:
-    print "Error: Could not import experiment."
-
-exp = experiment(session)
+# try:
+#     exp = imp.load_source('experiment', "wallace_experiment.py")
+#     classes = inspect.getmembers(exp, inspect.isclass)
+#     exps = [c for c in classes
+#             if (c[1].__bases__[0].__name__ in "Experiment")]
+#     this_experiment = exps[0][0]
+#     mod = __import__('wallace_experiment', fromlist=[this_experiment])
+#     experiment = getattr(mod, this_experiment)
+# except ImportError:
+    # print "Error: Could not import experiment."
 
 
 @scheduler.scheduled_job('interval', minutes=0.25)
 def check_db_for_missing_notifications():
+    # print("0")
+    # exp = experiment(session)
     print("1")
     participants = Participant.query.all()
     print participants
