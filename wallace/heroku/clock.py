1738c213c4b76eeb14a96440e149d3b02ce33380
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-01-21 10:21:10 -0800

    just change the status code

--- wallace/heroku/clock.py
+++ wallace/heroku/clock.py
@@ -105,70 +105,65 @@ I am busy with other matters.".format(datetime.now(), assignment_id, round(durat
                 server.sendmail(fromaddr, toaddr, msg.as_string())
                 server.quit()
             else:
-                # if it has not been submitted shut everything down
-                # first turn off autorecruit
-                host = os.environ['HOST']
-                host = host[:-len(".herokuapp.com")]
-                args = json.dumps({"auto_recruit": "false"})
-                headers = {
-                    "Accept": "application/vnd.heroku+json; version=3",
-                    "Content-Type": "application/json"
-                }
-                heroku_email_address = os.getenv('heroku_email_address')
-                heroku_password = os.getenv('heroku_password')
-                dum = requests.patch("https://api.heroku.com/apps/{}/config-vars".format(host),
-                                     data=args,
-                                     auth=(heroku_email_address, heroku_password),
-                                     headers=headers)
-                # then force expire the hit via boto
-                # print "blerghhhh!"
-                # try:
-                #     conn.expire_hit(hit_id)
-                # except:
-                #     import traceback
-                #     traceback.print_exc()
+                # # if it has not been submitted shut everything down
+                # # first turn off autorecruit
+                # host = os.environ['HOST']
+                # host = host[:-len(".herokuapp.com")]
+                # args = json.dumps({"auto_recruit": "false"})
+                # headers = {
+                #     "Accept": "application/vnd.heroku+json; version=3",
+                #     "Content-Type": "application/json"
+                # }
+                # heroku_email_address = os.getenv('heroku_email_address')
+                # heroku_password = os.getenv('heroku_password')
+                # requests.patch("https://api.heroku.com/apps/{}/config-vars".format(host),
+                #                data=args,
+                #                auth=(heroku_email_address, heroku_password),
+                #                headers=headers)
+                # # then force expire the hit via boto
+                # conn.expire_hit(hit_id)
 
-                # print "gghhhhrrrooooooop"
-                # and send the researcher an email to let them know
-                # send the researcher an email to let them know
-                username = os.getenv('wallace_email_username')
-                fromaddr = username + "@gmail.com"
-                email_password = os.getenv("wallace_email_key")
-                toaddr = config.get('HIT Configuration', 'contact_email_on_error')
+                # # send the researcher an email to let them know
+                # username = os.getenv('wallace_email_username')
+                # fromaddr = username + "@gmail.com"
+                # email_password = os.getenv("wallace_email_key")
+                # toaddr = config.get('HIT Configuration', 'contact_email_on_error')
 
-                msg = MIMEText("Dearest Friend,\n\nI am afraid I write to you with most grave tidings. \
-                    At {}, during a routine check of the usually most delightful participant data table, \
-                    I happened to notice that assignment {} has been taking longer than we were expecting. \
-                    I recall you had suggested {} minutes as an upper limit for what was an acceptable length \
-                    of time for each assignment, however this assignment had been underway for a shocking {} \
-                    minutes, a full {} minutes over your allowance. I immediately dispatched a \
-                    telegram to our mutual friends at AWS and they infact informed me that they had already send \
-                    us an {} notification which we must have failed to process, implying that the assignment had \
-                    not been successfully completed. Of course when the seriousness of this scenario dawned on me \
-                    I had to depend on my trusting walking stick for support: without the notification \
-                    I didn't know to remove the old assignment's data from the tables and AWS will have already sent \
-                    their replacement, meaning that the tables may already be in a most unsound state! \n\n\
-                    I am sorry to trouble you with this, however, I do not know how to proceed so rather than trying \
-                    to remedy the scenario myself, I have instead temporarily ceased operations by expiring the HIT \
-                    with the fellows at AWS and have refrained form posting any further invitations myself. Once you \
-                    see fit I would be most appreciative if you could attend to this issue with the caution, sensitivity \
-                    and intelligence for which I know you so well.\
-                    \n\nI remain your faithful and obedient servant,\nAlfred R. Wallace\
-                    \n\nP.S. Please do not respond to this message, \
-                    I am busy with other matters.".format(datetime.now(),
-                                                          assignment_id,
-                                                          round(duration/60),
-                                                          round(p_time/60),
-                                                          round((p_time-duration)/60)))
-                msg['Subject'] = "Most troubling news."
+                # msg = MIMEText("Dearest Friend,\n\nI am afraid I write to you with most grave tidings. \
+                #     At {}, during a routine check of the usually most delightful participant data table, \
+                #     I happened to notice that assignment {} has been taking longer than we were expecting. \
+                #     I recall you had suggested {} minutes as an upper limit for what was an acceptable length \
+                #     of time for each assignment, however this assignment had been underway for a shocking {} \
+                #     minutes, a full {} minutes over your allowance. I immediately dispatched a \
+                #     telegram to our mutual friends at AWS and they infact informed me that they had already send \
+                #     us an {} notification which we must have failed to process, implying that the assignment had \
+                #     not been successfully completed. Of course when the seriousness of this scenario dawned on me \
+                #     I had to depend on my trusting walking stick for support: without the notification \
+                #     I didn't know to remove the old assignment's data from the tables and AWS will have already sent \
+                #     their replacement, meaning that the tables may already be in a most unsound state! \n\n\
+                #     I am sorry to trouble you with this, however, I do not know how to proceed so rather than trying \
+                #     to remedy the scenario myself, I have instead temporarily ceased operations by expiring the HIT \
+                #     with the fellows at AWS and have refrained form posting any further invitations myself. Once you \
+                #     see fit I would be most appreciative if you could attend to this issue with the caution, sensitivity \
+                #     and intelligence for which I know you so well.\
+                #     \n\nI remain your faithful and obedient servant,\nAlfred R. Wallace\
+                #     \n\nP.S. Please do not respond to this message, \
+                #     I am busy with other matters.".format(datetime.now(),
+                #                                           assignment_id,
+                #                                           round(duration/60),
+                #                                           round(p_time/60),
+                #                                           round((p_time-duration)/60)))
+                # msg['Subject'] = "Most troubling news."
 
-                server = smtplib.SMTP('smtp.gmail.com:587')
-                server.starttls()
-                server.login(username, email_password)
-                server.sendmail(fromaddr, toaddr, msg.as_string())
-                server.quit()
+                # server = smtplib.SMTP('smtp.gmail.com:587')
+                # server.starttls()
+                # server.login(username, email_password)
+                # server.sendmail(fromaddr, toaddr, msg.as_string())
+                # server.quit()
 
-    if emergency is False:
-        print "No evidence of missing notifications :-)"
+                # set the participants status to something >100 as otherwise the
+                # emergency method will run repeatedly
+                p.status = 666
+                db.commit()
 
 scheduler.start()
