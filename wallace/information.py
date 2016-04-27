d04be03be70ce3d336827ffbef64ff525841250d
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-04-27 15:16:30 -0700

    lint information.py

--- wallace/information.py
+++ wallace/information.py
@@ -4,12 +4,18 @@ from .models import Info
 
 
 class Gene(Info):
+    """A gene."""
+
     __mapper_args__ = {"polymorphic_identity": "gene"}
 
 
 class Meme(Info):
+    """A meme."""
+
     __mapper_args__ = {"polymorphic_identity": "meme"}
 
 
 class State(Info):
+    """A state."""
+
     __mapper_args__ = {"polymorphic_identity": "state"}
