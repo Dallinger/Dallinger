332093101c48293c1853371f248b4f19a594175d
Jordan Suchow <suchow@post.harvard.edu>
2016-08-08 20:24:22 -0400

    Improve the build script

--- scripts/build-demo-docs.sh
+++ scripts/build-demo-docs.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+mkdir -p docs/demos/
 mkdir -p docs/demos/assets
 mkdir -p build
-rm -r build/*
 
 cd examples
 
 for filename in *; do
     echo $filename
+
+    # Clean needless files.
+    rm $filename/-
+    rm $filename/.psiturk_history
+    rm -r $filename/snapshots/*
+
+    # Copy over the README.
     cp $filename/README.md ../docs/demos/"$filename.md"
+
+    # Zip up the demo.
     zip -r "$filename.zip" $filename
     cp "$filename.zip" ../build/"$filename.zip"
     cp "$filename.zip" ../docs/demos/assets/"$filename.zip"
@@ -16,5 +25,8 @@ for filename in *; do
 done
 
 cd ..
+
+# rm -r build/*
+# rm -r docs/demos/*
 
 mkdocs gh-deploy --clean --verbose
