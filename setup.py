62fc72abf788fdcb8e05e801cd5cb27851ff4f54
Jordan Suchow <suchow@post.harvard.edu>
2016-08-18 01:25:29 -0400

    Bump version to 0.12.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup_args = dict(
     name='wallace-platform',
     packages=['wallace'],
-    version="0.12.1",
+    version="0.12.2",
     description='Wallace, a platform for experimental cultural evolution',
     url='http://github.com/berkeley-cocosci/Wallace',
     author='Berkeley CoCoSci',
--- wallace/version.py
+++ wallace/version.py
@@ -1,3 +1,3 @@
 """Wallace version number."""
 
-__version__ = "0.12.1"
+__version__ = "0.12.2"
