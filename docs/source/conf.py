c5c2c6304f8df3e4d1f2e80c049dab66186c2e60
Jordan Suchow <suchow@post.harvard.edu>
2016-08-22 02:10:52 -0400

    Hide navigation in sidebar (#469)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -206,7 +206,7 @@ html_static_path = ['_static']
 html_sidebars = {
     '**': [
         'about.html',
-        'navigation.html',
+        # 'navigation.html',
         'relations.html',
         'searchbox.html',
         'donate.html',
