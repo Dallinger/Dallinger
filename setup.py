246215d71bb5ea12705f60ab90a5d3a1b15442e3
Jordan Suchow <suchow@post.harvard.edu>
2016-09-07 00:20:44 -0400

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', 'w') as outfile:
 setup_args = dict(
     name='dallinger',
     packages=['dallinger'],
-    version="1.0.0",
+    version="2.0.0",
     description='Laboratory automation for the behavioral and social sciences',
     long_description=long_description,
     url='http://github.com/Dallinger/Dallinger',
