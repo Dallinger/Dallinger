696bfec68539d239d4923f8acd04dc732e8b56ae
Jordan Suchow <suchow@post.harvard.edu>
2016-09-09 11:51:16 -0400

    Add new demo on Vox Populi

---
+++ examples/vox-populi/README.md
@@ -0,0 +1,5 @@
+# The Sheep Market
+
+"The Sheep Market is a collection of 10,000 sheep created by workers on Amazon’s Mechanical Turk. Each worker was paid $.02 (US) to “draw a sheep facing left."
+
+http://www.aaronkoblin.com/project/the-sheep-market/
---
+++ examples/vox-populi/static/scripts/experiment.js
@@ -0,0 +1,57 @@
+// Create the agent.
+create_agent = function() {
+    reqwest({
+        url: "/node/" + participant_id,
+        method: 'post',
+        type: 'json',
+        success: function (resp) {
+            my_node_id = resp.node.id;
+            $("#canvas").hide();
+            $("#response-form").show();
+            $("#submit-response").removeClass('disabled');
+            $("#submit-response").html('Submit');
+            sketchpad = Raphael.sketchpad("editor", {
+                width: 300,
+                height: 300,
+                editing: true
+            });
+            pen = sketchpad.pen();
+            pen.width(2);
+            $("#editor").width(300).height(300);
+        },
+        error: function (err) {
+            console.log(err);
+            err_response = JSON.parse(err.response);
+            if (err_response.hasOwnProperty('html')) {
+                $('body').html(err_response.html);
+            } else {
+                allow_exit();
+                go_to_page('questionnaire');
+            }
+        }
+    });
+};
+
+submit_response = function() {
+
+    canvg('canvas', $("#editor").html());
+    console.log(canvas.toDataURL("image/png"));
+
+    $("#submit-response").addClass('disabled');
+    $("#submit-response").html('Sending...');
+
+    reqwest({
+        url: "/info/" + my_node_id,
+        method: 'post',
+        data: {
+            contents: JSON.stringify({
+                "sketch": sketchpad.json(),
+                "image": canvas.toDataURL("image/png"),
+            }),
+            info_type: "Info"
+        },
+        success: function (resp) {
+            create_agent();
+        }
+    });
+};
