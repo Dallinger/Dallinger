7a2cb5a2c529d0d502ab1da30d7f0ebb9a2c1966
Jordan Suchow <suchow@post.harvard.edu>
2016-09-26 13:44:28 -0400

    Clean up Bartlett 1932 demo (#225)

--- demos/bartlett1932/static/scripts/experiment.js
+++ demos/bartlett1932/static/scripts/experiment.js
@@ -1,3 +1,74 @@
+// Consent to the experiment.
+$(document).ready(function() {
+
+    // Print the consent form.
+    $("#print-consent").click(function() {
+        console.log("hello");
+        window.print();
+    });
+
+    // Consent to the experiment.
+    $("#consent").click(function() {
+        store.set("hit_id", getUrlParameter("hit_id"));
+        store.set("worker_id", getUrlParameter("worker_id"));
+        store.set("assignment_id", getUrlParameter("assignment_id"));
+        store.set("mode", getUrlParameter("mode"));
+
+        allow_exit();
+        window.location.href = '/instructions';
+    });
+
+    // Consent to the experiment.
+    $("#no-consent").click(function() {
+        allow_exit();
+        self.close();
+    });
+
+    // Consent to the experiment.
+    $("#go-to-experiment").click(function() {
+        allow_exit();
+        window.location.href = '/exp';
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+
+    $("#finish-reading").click(function() {
+        $("#stimulus").hide();
+        $("#response-form").show();
+        $("#submit-response").removeClass('disabled');
+        $("#submit-response").html('Submit');
+    });
+
+    $("#submit-response").click(function() {
+        $("#submit-response").addClass('disabled');
+        $("#submit-response").html('Sending...');
+
+        response = $("#reproduction").val();
+
+        $("#reproduction").val("");
+
+        reqwest({
+            url: "/info/" + my_node_id,
+            method: 'post',
+            data: {
+                contents: response,
+                info_type: "Info"
+            },
+            success: function (resp) {
+                create_agent();
+            }
+        });
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+});
+
 // Create the agent.
 create_agent = function() {
     reqwest({
@@ -38,34 +109,6 @@ get_info = function() {
             console.log(err);
             errorResponse = JSON.parse(err.response);
             $('body').html(errorResponse.html);
-        }
-    });
-};
-
-finish_reading = function() {
-    $("#stimulus").hide();
-    $("#response-form").show();
-    $("#submit-response").removeClass('disabled');
-    $("#submit-response").html('Submit');
-};
-
-submit_response = function() {
-    $("#submit-response").addClass('disabled');
-    $("#submit-response").html('Sending...');
-
-    response = $("#reproduction").val();
-
-    $("#reproduction").val("");
-
-    reqwest({
-        url: "/info/" + my_node_id,
-        method: 'post',
-        data: {
-            contents: response,
-            info_type: "Info"
-        },
-        success: function (resp) {
-            create_agent();
         }
     });
 };
