05eabf545db4b29e735f30f21d455aa73240746e
Jordan Suchow <suchow@post.harvard.edu>
2016-11-02 21:57:08 -0400

    Extract dallinger.heroku.log_in from CLI

--- dallinger/heroku/tools.py
+++ dallinger/heroku/tools.py
@@ -1,5 +1,14 @@
 """Miscellaneous tools for Heroku."""
 
+import pexpect
+
+
 def app_name(id):
     """Convert a UUID to a valid Heroku app name."""
     return "dlgr-" + id[0:8]
+
+
+def log_in():
+    """Ensure that the user is logged in to Heroku."""
+    p = pexpect.spawn("heroku auth:whoami")
+    p.interact()
