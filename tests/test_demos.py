e5237ef85f63b4bf6b3bff9db221bf6cec9357d8
Jesse Snyder <jesse@rasikaconsulting.com>
2016-12-13 16:13:36 -0800

    Make the Bartlett1932 demo a python package

---
+++ tests/test_demos.py
@@ -0,0 +1,25 @@
+from dallinger import db
+
+
+class TestBartlett1932(object):
+    """Tests for the Bartlett1932 demo class"""
+
+    def _make_one(self):
+        from demos import Bartlett1932
+        return Bartlett1932(self._db)
+
+    def setup(self):
+        self._db = db.init_db(drop_all=True)
+
+    def teardown(self):
+        self._db.rollback()
+        self._db.close()
+
+    def test_instantiation(self):
+        demo = self._make_one()
+        assert demo is not None
+
+    def test_networks_holds_single_experiment_node(self):
+        demo = self._make_one()
+        assert len(demo.networks()) == 1
+        assert u'experiment' == demo.networks()[0].role
