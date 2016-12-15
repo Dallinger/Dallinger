7a0ea501c7eb9f2ab63f96ef9dc0b4a8b674e961
Alec Mitchell <alecpm@gmail.com>
2016-12-14 16:06:15 -0800

    Add a dallinger.experiment entry point group

--- dallinger/__init__.py
+++ dallinger/__init__.py
@@ -11,8 +11,30 @@ from . import (
     heroku
 )
 
+import logging
+from logging import NullHandler
+from pkg_resources import iter_entry_points
+from types import ModuleType
+import sys
 from localconfig import config
 config.read("config.txt")
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
+experiment = ModuleType(
+    'experiment',
+    'A module where Dallinger experiments are registered'
+)
+for entry_point in iter_entry_points(group='dallinger.experiment'):
+    try:
+        setattr(experiment, entry_point.name, entry_point.load())
+    except ImportError:
+        logger.warn('Could not import registered entry point {}'.format(
+            entry_point.name
+        ))
+
+sys.modules['dallinger.experiment'] = experiment
+
 
 __all__ = (
     "config",
@@ -24,4 +46,5 @@ __all__ = (
     "transformations",
     "experiments",
     "heroku",
+    "experiment",
 )
