baa74c32d34671b9603b1ef4ecce354347e609d7
jessesnyder <jesse@rasikaconsulting.com>
2017-01-02 14:54:11 -0800

    Remove vestigial code spike and tests

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -5,7 +5,7 @@ class TestBartlett1932(object):
     """Tests for the Bartlett1932 demo class"""
 
     def _make_one(self):
-        from demos.bartlett1932 import Bartlett1932
+        from demos.bartlett1932.experiment import Bartlett1932
         return Bartlett1932(self._db)
 
     def setup(self):
@@ -23,28 +23,6 @@ class TestBartlett1932(object):
         demo = self._make_one()
         assert len(demo.networks()) == 1
         assert u'experiment' == demo.networks()[0].role
-
-
-class TestBartlett1932DemoFactory(object):
-    """Just a sketch."""
-
-    def _make_one(self):
-        from demos.bartlett1932 import build
-        return build()
-
-    def setup(self):
-        self._db = db.init_db(drop_all=True)
-
-    def teardown(self):
-        self._db.rollback()
-        self._db.close()
-
-    def test_demo_factory(self):
-        from demos.bartlett1932.experiment import Bartlett1932
-        demo = self._make_one()
-
-        assert demo is not None
-        assert isinstance(demo, Bartlett1932)
 
 
 class TestEntryPointImport(object):
