26ad31fed32e939aed666495b659ba99adfcc6bb
jessesnyder <jesse@rasikaconsulting.com>
2017-01-03 12:04:12 -0800

    Skeleton implementation of MTurkRecruiter, with placeholders for methods which need native boto implementations to replace Psiturk ones.

--- tests/test_recruiters.py
+++ tests/test_recruiters.py
@@ -29,3 +29,29 @@ class TestRecruiters(object):
     def test_recruiter_simulated(self):
         from dallinger.recruiters import SimulatedRecruiter
         assert SimulatedRecruiter()
+
+
+class TestMTurkRecruiter(object):
+
+    def setup(self):
+        # XXX Ugh.
+        self.db = db.init_db(drop_all=True)
+        os.chdir(os.path.join("demos", "bartlett1932"))
+
+    def teardown(self):
+        # XXX Ugh.
+        self.db.rollback()
+        self.db.close()
+        os.chdir("..")
+        os.chdir("..")
+
+    def test_instantiation(self):
+        from dallinger.recruiters import MTurkRecruiter
+        recruiter = MTurkRecruiter()
+        assert recruiter is not None
+
+    def test_open_recruitment(self):
+        from dallinger.recruiters import MTurkRecruiter
+        recruiter = MTurkRecruiter()
+        hit_info = recruiter.open_recruitment(n=1)
+        assert 'hit_id' in hit_info
