bfc6742715f5d6f23b2b98e420f584270026a393
Jordan Suchow <suchow@post.harvard.edu>
2017-01-03 02:32:45 -0500

    Initial commit of image labeling demo

---
+++ demos/image-labeling/experiment.py
@@ -0,0 +1,78 @@
+"""Bartlett's trasmission chain experiment from Remembering (1932)."""
+
+from dallinger.networks import Chain
+from dallinger.nodes import Source
+from dallinger.experiments import Experiment
+import random
+
+
+class Bartlett1932(Experiment):
+    """Define the structure of the experiment."""
+
+    def __init__(self, session):
+        """Call the same function in the super (see experiments.py in dallinger).
+
+        A few properties are then overwritten.
+        Finally, setup() is called.
+        """
+        super(Bartlett1932, self).__init__(session)
+        self.experiment_repeats = 1
+        self.setup()
+
+    def setup(self):
+        """Setup the networks.
+
+        Setup only does stuff if there are no networks, this is so it only
+        runs once at the start of the experiment. It first calls the same
+        function in the super (see experiments.py in dallinger). Then it adds a
+        source to each network.
+        """
+        if not self.networks():
+            super(Bartlett1932, self).setup()
+            for net in self.networks():
+                WarOfTheGhostsSource(network=net)
+
+    def create_network(self):
+        """Return a new network."""
+        return Chain(max_size=3)
+
+    def add_node_to_network(self, node, network):
+        """Add node to the chain and receive transmissions."""
+        network.add_node(node)
+        parent = node.neighbors(direction="from")[0]
+        parent.transmit()
+        node.receive()
+
+    def recruit(self):
+        """Recruit one participant at a time until all networks are full."""
+        if self.networks(full=False):
+            self.recruiter().recruit_participants(n=1)
+        else:
+            self.recruiter().close_recruitment()
+
+
+class WarOfTheGhostsSource(Source):
+    """A Source that reads in a random story from a file and transmits it."""
+
+    __mapper_args__ = {
+        "polymorphic_identity": "war_of_the_ghosts_source"
+    }
+
+    def _contents(self):
+        """Define the contents of new Infos.
+
+        transmit() -> _what() -> create_information() -> _contents().
+        """
+        stories = [
+            "ghosts.md",
+            "cricket.md",
+            "moochi.md",
+            "outwit.md",
+            "raid.md",
+            "species.md",
+            "tennis.md",
+            "vagabond.md"
+        ]
+        story = random.choice(stories)
+        with open("static/stimuli/{}".format(story), "r") as f:
+            return f.read()
---
+++ demos/image-labeling/static/scripts/experiment.js
@@ -0,0 +1,114 @@
+// Consent to the experiment.
+$(document).ready(function() {
+
+    // Print the consent form.
+    $("#print-consent").click(function() {
+        console.log("hello");
+        window.print();
+    });
+
+    // Consent to the experiment.
+    $("#consent").click(function() {
+        store.set("hit_id", getUrlParameter("hit_id"));
+        store.set("worker_id", getUrlParameter("worker_id"));
+        store.set("assignment_id", getUrlParameter("assignment_id"));
+        store.set("mode", getUrlParameter("mode"));
+
+        allow_exit();
+        window.location.href = '/instructions';
+    });
+
+    // Consent to the experiment.
+    $("#no-consent").click(function() {
+        allow_exit();
+        self.close();
+    });
+
+    // Consent to the experiment.
+    $("#go-to-experiment").click(function() {
+        allow_exit();
+        window.location.href = '/exp';
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+
+    $("#finish-reading").click(function() {
+        $("#stimulus").hide();
+        $("#response-form").show();
+        $("#submit-response").removeClass('disabled');
+        $("#submit-response").html('Submit');
+    });
+
+    $("#submit-response").click(function() {
+        $("#submit-response").addClass('disabled');
+        $("#submit-response").html('Sending...');
+
+        response = $("#reproduction").val();
+
+        $("#reproduction").val("");
+
+        reqwest({
+            url: "/info/" + my_node_id,
+            method: 'post',
+            data: {
+                contents: response,
+                info_type: "Info"
+            },
+            success: function (resp) {
+                create_agent();
+            }
+        });
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+});
+
+// Create the agent.
+create_agent = function() {
+    reqwest({
+        url: "/node/" + participant_id,
+        method: 'post',
+        type: 'json',
+        success: function (resp) {
+            my_node_id = resp.node.id;
+            get_info(my_node_id);
+        },
+        error: function (err) {
+            console.log(err);
+            errorResponse = JSON.parse(err.response);
+            if (errorResponse.hasOwnProperty('html')) {
+                $('body').html(errorResponse.html);
+            } else {
+                allow_exit();
+                go_to_page('questionnaire');
+            }
+        }
+    });
+};
+
+get_info = function() {
+    reqwest({
+        url: "/node/" + my_node_id + "/received_infos",
+        method: 'get',
+        type: 'json',
+        success: function (resp) {
+            story = resp.infos[0].contents;
+            storyHTML = markdown.toHTML(story);
+            $("#story").html(storyHTML);
+            $("#stimulus").show();
+            $("#response-form").hide();
+            $("#finish-reading").show();
+        },
+        error: function (err) {
+            console.log(err);
+            errorResponse = JSON.parse(err.response);
+            $('body').html(errorResponse.html);
+        }
+    });
+};
