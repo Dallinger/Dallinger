d8946e1595a0b80a1096770ed182a4b373e95047
jessesnyder <jesse@rasikaconsulting.com>
2017-01-05 18:53:16 -0800

    Factor out boto-related code into a helper class, tie things back together and get the recruiter working.

--- tests/test_recruiters.py
+++ tests/test_recruiters.py
@@ -37,23 +37,23 @@ class TestRecruiters(object):
 
 def stub_config(**kwargs):
     defaults = {
+        'ad_url': 'https://url-of-ad-route',
         'aws_access_key_id': 'fake key',
         'aws_secret_access_key': 'fake secret',
         'launch_in_sandbox_mode': True,
         'base_payment': 0.01,
         'duration': 1.0,
         'server': '0.0.0.0',
-        'browser_exclude_rule': 'fakebrowser1, fakebrowser2',
+        'browser_exclude_rule': ['fakebrowser1', 'fakebrowser2'],
         'organization_name': 'fake org name',
-        'notification_url': 'fake notification url',
-        'contact_email_on_error': 'fake@fake.com',
+        'notification_url': 'https://url-of-notification-route',
         'ad_group': 'fake ad group',
         'approve_requirement': 95,
         'us_only': True,
         'lifetime': 1.0,
         'title': 'fake experiment title',
         'description': 'fake HIT description',
-        'keywords': 'kw1, kw2, kw3',
+        'keywords': ['kw1', 'kw2', 'kw3'],
     }
     defaults.update(kwargs)
 
@@ -106,75 +106,94 @@ class TestMTurkRecruiter(object):
         recruiter = self.make_one()
         assert recruiter.config.get('title') == 'fake experiment title'
 
-    def test_check_aws_credentials_good_credentials(self):
+    def test_open_recruitment_single_recruitee(self):
         recruiter = self.make_one(**creds_from_environment())
-        is_authenticated = recruiter.check_aws_credentials()
+        hit_info = recruiter.open_recruitment(n=1)
+        assert hit_info['assignments_available'] == 1
+
+    def test_open_recruitment_two_recruitees(self):
+        recruiter = self.make_one(**creds_from_environment())
+        hit_info = recruiter.open_recruitment(n=2)
+        assert hit_info['assignments_available'] == 2
+
+
+class TestMTurkService(object):
+
+    def setup(self):
+        self.db = db.init_db(drop_all=True)
+
+    def teardown(self):
+        self.db.rollback()
+        self.db.close()
+
+    def make_one(self, **kwargs):
+        from dallinger.recruiters import MTurkService
+        return MTurkService(**kwargs)
+
+    def test_check_credentials_good_credentials(self):
+        service = self.make_one(**creds_from_environment())
+        is_authenticated = service.check_credentials()
+
         assert is_authenticated
 
-    def test_check_aws_credentials_bad_credentials(self):
+    def test_check_credentials_bad_credentials(self):
         from boto.mturk.connection import MTurkRequestError
-        recruiter = self.make_one()
+        service = self.make_one(aws_access_key_id='bad', aws_secret_access_key='bad')
         with assert_raises(MTurkRequestError):
-            recruiter.check_aws_credentials()
+            service.check_credentials()
 
-    def test_check_aws_credentials_no_creds_set_raises(self):
-        from dallinger.recruiters import MTurkRecruiterException
+    def test_check_credentials_no_creds_set_raises(self):
+        from dallinger.recruiters import MTurkServiceException
         empty_creds = {
             'aws_access_key_id': '',
             'aws_secret_access_key': ''
         }
-        recruiter = self.make_one(**empty_creds)
+        service = self.make_one(**empty_creds)
 
-        with assert_raises(MTurkRecruiterException):
-            recruiter.check_aws_credentials()
+        with assert_raises(MTurkServiceException):
+            service.check_credentials()
 
     def test_register_hit_type(self):
         config = {
-            "title": 'Test Title',
-            "description": 'Test Description',
-            "keywords": ['testkw1', 'testkw1'],
-            "reward": Price(.01),
-            "duration": datetime.timedelta(hours=.25)
+            'title': 'Test Title',
+            'description': 'Test Description',
+            'keywords': ['testkw1', 'testkw1'],
+            'reward': .01,
+            'duration': .25
         }
-        recruiter = self.make_one(**creds_from_environment())
-        hit_type = recruiter.register_hit_type(config)
-        assert hasattr(hit_type, 'HITTypeId')
+        service = self.make_one(**creds_from_environment())
+        hit_type_id = service.register_hit_type(**config)
+
+        assert isinstance(hit_type_id, unicode)
 
     def test_register_notification_url(self):
         config = {
-            "title": 'Test Title',
-            "description": 'Test Description',
-            "keywords": ['testkw1', 'testkw1'],
-            "reward": Price(.01),
-            "duration": datetime.timedelta(hours=.25)
+            'title': 'Test Title',
+            'description': 'Test Description',
+            'keywords': ['testkw1', 'testkw1'],
+            'reward': .01,
+            'duration': .25
         }
-        url = 'https://url-of-real-ad-route'
-        recruiter = self.make_one(**creds_from_environment())
-        hit_type = recruiter.register_hit_type(config)
+        url = 'https://url-of-notification-route'
+        service = self.make_one(**creds_from_environment())
+        hit_type_id = service.register_hit_type(**config)
 
-        result = recruiter.register_notification_url(url, hit_type.HITTypeId)
-
-        assert 'NONSENSE' in result
+        assert service.set_rest_notification(url, hit_type_id) is True
 
     def test_create_hit(self):
         hit_config = {
-            "ad_url": 'https://www.google.com/',  # ???
-            "approve_requirement": 95,
-            "us_only": True,
-            "lifetime": datetime.timedelta(hours=1),
-            "max_assignments": 1,
-            "notification_url": 'fake notification url',
-            "title": 'Test Title',
-            "description": 'Test Description',
-            "keywords": ['testkw1', 'testkw1'],
-            "reward": Price(.01),
-            "duration": datetime.timedelta(hours=.25)
+            'ad_url': 'https://url-of-ad-route',
+            'approve_requirement': 95,
+            'us_only': True,
+            'lifetime': 1.0,
+            'max_assignments': 1,
+            'notification_url': 'https://url-of-notification-route',
+            'title': 'Test Title',
+            'description': 'Test Description',
+            'keywords': ['testkw1', 'testkw1'],
+            'reward': .01,
+            'duration': .25
         }
-        recruiter = self.make_one(**creds_from_environment())
-        hit = recruiter.create_hit(hit_config)
-        assert 'hit_id' in hit
-
-    # def test_open_recruitment(self):
-    #     recruiter = self.make_one(**creds_from_environment())
-    #     hit_info = recruiter.open_recruitment(n=1)
-    #     assert 'hit_id' in hit_info
+        service = self.make_one(**creds_from_environment())
+        hit = service.create_hit(**hit_config)
+        assert hit['status'] == 'Assignable'
