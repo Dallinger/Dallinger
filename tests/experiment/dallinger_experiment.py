7cf680544e93f7d7d1d0570dc11aaf6758f3bf17
Alec Mitchell <alecpm@gmail.com>
2017-01-06 12:20:44 -0800

    Move status to /summary. Add more tests

--- tests/experiment/dallinger_experiment.py
+++ tests/experiment/dallinger_experiment.py
@@ -1,11 +1,20 @@
 from dallinger.config import get_config
 from dallinger.experiments import Experiment
+from dallinger.networks import Star
 
 config = get_config()
 
 
 class TestExperiment(Experiment):
-    pass
+
+    def __init__(self, session=None):
+        super(TestExperiment, self).__init__(session)
+        self.experiment_repeats = 1
+        self.setup()
+
+    def create_network(self):
+        """Return a new network."""
+        return Star(max_size=2)
 
 
 def extra_settings():
