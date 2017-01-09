614f22941024bbbd3ddcf51ebeb5c6b9b2475657
David Glick <david@glicksoftware.com>
2017-01-08 23:35:00 -0800

    Run docs first in tox so it doesn't obscure coverage result

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import os
 import pytest
 
 
+# This fixture is used automatically and ensures that
+# the current working directory is reset if other test classes changed it.
 @pytest.fixture(scope="class")
 def cwd():
     root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
