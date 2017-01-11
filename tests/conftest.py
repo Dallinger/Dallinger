eb18dd3431d08d01ffa4da7653c4d0b502318f94
jessesnyder <jesse@rasikaconsulting.com>
2017-01-11 14:06:53 -0800

    Fix bad names

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,7 @@ def creds_from_environment():
 
 # decorator for test methods or classes which should be skipped if there
 # are no aws credentials set in the environment
-skip_if_no_aws_credentials = pytest.mark.skipif(
+skip_if_no_mturk_requestor = pytest.mark.skipif(
     not os.getenv('is_aws_mturk_requestor') and
     creds_from_environment().values(),
     reason="Not configured to run Amazon MTurk system tests."
