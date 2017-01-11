0edd34522097cb18a7c5e518599ecea4341261a1
jessesnyder <jesse@rasikaconsulting.com>
2017-01-10 16:09:42 -0800

    Bring recruiters.py up to date post-rebase.

--- dallinger/recruiters.py
+++ dallinger/recruiters.py
@@ -1,7 +1,8 @@
 """Recruiters manage the flow of participants to the experiment."""
-
 from boto.mturk.connection import MTurkConnection
 from dallinger.config import get_config
+from dallinger.models import Participant
+from dallinger.mturk import MTurkService
 from dallinger.utils import get_base_url
 from dallinger.utils import generate_random_id
 import logging
@@ -99,6 +100,9 @@ class PsiTurkRecruiter(Recruiter):
                 return 'yes'
 
         config = get_config()
+        if not config.ready:
+            config.load_config()
+
         self.server = FakeExperimentServerController()
 
         # Get keys from environment variables or config file.
@@ -229,159 +233,56 @@ class PsiTurkRecruiter(Recruiter):
         pass
 
 
-# XXX Move imports
-import sys
-import datetime
-from boto.mturk.price import Price
-
-
-class ConfigurationWrapper(object):
-    """Temporary, until we get the real thing."""
-
-    def __init__(self):
-        # load the configuration options from various places in provided a
-        # uniform interface to them.
-        self._data = {}
-        self.config = PsiturkConfig()
-        self.config.load_config()
-
-        # Get keys from environment variables or config file.
-        self._data['aws_access_key_id'] = os.getenv(
-            "aws_access_key_id",
-            self.config.get("AWS Access", "aws_access_key_id"))
-        self._data['aws_secret_access_key'] = os.getenv(
-            "aws_secret_access_key",
-            self.config.get("AWS Access", "aws_secret_access_key"))
-        self._data['aws_region'] = os.getenv(
-            "aws_region",
-            self.config.get("AWS Access", "aws_region"))
-        self._data['is_sandbox'] = self.config.getboolean(
-            'Shell Parameters', 'launch_in_sandbox_mode')
-        self._data['base_payment'] = self.config.get('HIT Configuration', 'base_payment')
-        self._data['duration'] = self.config.getfloat('HIT Configuration', 'duration')
-
-        self._data['server'] = os.getenv('HOST', self.config.get("Server Parameters", "host"))
-        self._data['browser_exclude_rule'] = str(
-            self.config.get('HIT Configuration', 'browser_exclude_rule'))
-        self._data['organization_name'] = str(
-            self.config.get('HIT Configuration', 'organization_name'))
-        self._data['experiment_name'] = str(self.config.get('HIT Configuration', 'title'))
-        self._data['contact_email_on_error'] = str(
-            self.config.get('HIT Configuration', 'contact_email_on_error'))
-        self._data['ad_group'] = str(self.config.get('HIT Configuration', 'ad_group'))
-        self._data['approve_requirement'] = self.config.get(
-            'HIT Configuration', 'Approve_Requirement')
-        self._data['us_only'] = self.config.getboolean('HIT Configuration', 'US_only')
-        self._data['lifetime'] = self.config.getfloat('HIT Configuration', 'lifetime')
-        self._data['description'] = self.config.get('HIT Configuration', 'description')
-        self._data['keywords'] = self.config.get('HIT Configuration', 'amt_keywords')
-
-    def get(self, key):
-        return self._data[key]
-
-
 class MTurkRecruiterException(Exception):
     """Custom exception for MTurkRecruiter"""
 
 
 class MTurkRecruiter(object):
-    """Recruit participants from Amazon Mechanical Turk via boto"""
-
-    ad_file_max_bytes = 1048576
-    production_mturk_server = 'mechanicalturk.amazonaws.com'
-    sandbox_mturk_server = 'mechanicalturk.sandbox.amazonaws.com'
-    ad_url = 'do we need this? what is this?'
+    """Recruit participants from Amazon Mechanical Turk"""
 
     @classmethod
     def from_current_config(cls):
-        config = ConfigurationWrapper()
+        config = get_config()
+        if not config.ready:
+            config.load_config()
         return cls(config)
 
     def __init__(self, config):
         self.config = config
-        self.aws_access_key_id = self.config.get('aws_access_key_id')
-        self.aws_secret_access_key = self.config.get('aws_secret_access_key')
-        self.aws_region = self.config.get('aws_region')
-        self.is_sandbox = self.config.get('launch_in_sandbox_mode')
-        self.reward = Price(self.config.get('base_payment'))
-        self.duration = datetime.timedelta(hours=self.config.get('duration'))
+        self.mturkservice = MTurkService(
+            self.config.get('aws_access_key_id'),
+            self.config.get('aws_secret_access_key'),
+            self.config.get('launch_in_sandbox_mode')
+        )
 
     def open_recruitment(self, n=1):
         """Open a connection to AWS MTurk and create a HIT."""
-        max_assignments = n
-        if self.have_participants():
+        if self.is_in_progress:
             # Already started... do nothing.
             return
 
         if self.config.get('server') in ['localhost', '127.0.0.1']:
-            print "Can't run real HIT from localhost"
-            return
+            raise MTurkRecruiterException("Can't run a HIT from localhost")
 
-        # Check AWS credentials
-        if not self.check_aws_credentials():
-            print 'Invalid AWS credentials.'
-            return
+        self.mturkservice.check_credentials()
 
-        hit_config = {
-            "ad_location": self.ad_url,
-            "approve_requirement": self.config.get('approve_requirement'),
-            "us_only": self.config.get('us_only'),
-            "lifetime": self.config.get('lifetime'),
-            "max_assignments": max_assignments,
-            "title": self.config.get('experiment_title'),
-            "description": self.config.get('description'),
-            "keywords": self.config.get('amt_keywords'),
-            "reward": self.reward,
-            "duration": self.duration
+        hit_request = {
+            'max_assignments': n,
+            'title': self.config.get('title'),
+            'description': self.config.get('description'),
+            'keywords': self.config.get('keywords'),
+            'reward': self.config.get('base_payment'),
+            'duration': self.config.get('duration'),
+            'lifetime': self.config.get('lifetime'),
+            'ad_url': self.config.get('ad_url'),
+            'notification_url': self.config.get('notification_url'),
+            'approve_requirement': self.config.get('approve_requirement'),
+            'us_only': self.config.get('us_only'),
         }
-        hit_id = self.create_hit(hit_config)
-        if hit_id is False:
-            print "Unable to create HIT on Amazon Mechanical Turk."
-            return
+        hit_info = self.mturkservice.create_hit(**hit_request)
 
-        report = {
-            'hit_id': hit_id,
-            'duration': self.duration,
-            'workers': max_assignments,
-            'reward': self.reward,
-            'environment': self.is_sandbox and 'sandbox' or 'live'
-        }
-
-        return report
+        return hit_info
 
     @property
-    def host(self):
-        if self.is_sandbox:
-            return self.sandbox_mturk_server
-        return self.production_mturk_server
-
-    def have_participants(self):
-        from dallinger.models import Participant
+    def is_in_progress(self):
         return bool(Participant.query.all())
-
-    def create_hit(self, hit_confg):
-        # Replicate psiturk.amt_services.MTurkServices.create_hit()
-        return 'some HIT ID'
-
-    def update_ad_with_hit_id(self, ad_id, hit_id):
-        # Replicate PsiturkOrgServices.set_ad_hitid()
-        return True
-
-    def check_aws_credentials(self):
-        """Verifies key/secret/host combination by making a balance inquiry"""
-        if not self.aws_access_key_id or not self.aws_secret_access_key:
-            raise MTurkRecruiterException('AWS access key and secret not set.')
-
-        mturkparams = {
-            'aws_access_key_id': self.aws_access_key_id,
-            'aws_secret_access_key': self.aws_secret_access_key,
-            'host': self.host
-        }
-        mtc = MTurkConnection(**mturkparams)
-        try:
-            mtc.get_account_balance()
-        except MTurkRequestError as exception:
-            print exception.error_message
-            return False
-        else:
-            return True
