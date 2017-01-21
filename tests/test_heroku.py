bf8c573701e2ffb85a74864cc574d158d7c1dad1
jessesnyder <jesse@rasikaconsulting.com>
2017-01-20 18:17:25 -0800

    Remove unused test methods

--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -8,17 +8,6 @@ from dallinger.heroku import app_name
 
 class TestHeroku(object):
 
-    def setup(self):
-        """Set up the environment by moving to the demos directory."""
-        os.chdir("demos")
-
-    def teardown(self):
-        os.chdir("..")
-
-    def add(self, *args):
-        self.db.add_all(args)
-        self.db.commit()
-
     def test_heroku_app_name(self):
         id = "8fbe62f5-2e33-4274-8aeb-40fc3dd621a0"
         assert(len(app_name(id)) < 30)
