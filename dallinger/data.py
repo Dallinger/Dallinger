a1378b1bc5d4d08cb6462b678ae2e42656c3453d
Jordan Suchow <suchow@post.harvard.edu>
2017-01-22 17:38:13 -0500

    Define table names in data module

---
+++ dallinger/data.py
@@ -0,0 +1,13 @@
+"""Data-handling tools."""
+
+table_names = [
+    "info",
+    "network",
+    "node",
+    "notification",
+    "participant",
+    "question",
+    "transformation",
+    "transmission",
+    "vector",
+]
