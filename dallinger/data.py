ba90d970729ff149e48744aaa50e0fc0fc854941
Jordan Suchow <suchow@post.harvard.edu>
2017-01-22 17:40:03 -0500

    Define a Data object and CSV converters

--- dallinger/data.py
+++ dallinger/data.py
@@ -1,5 +1,14 @@
 """Data-handling tools."""
 
+import os
+import tempfile
+from zipfile import ZipFile
+
+import odo
+import pandas as pd
+import tablib
+
+
 table_names = [
     "info",
     "network",
@@ -11,3 +20,88 @@ table_names = [
     "transmission",
     "vector",
 ]
+
+
+class Data(object):
+    """Dallinger data object."""
+    def __init__(self, URL):
+        super(Data, self).__init__()
+
+        self.source = URL
+
+        if self.source.endswith(".zip"):
+
+            input_zip = ZipFile(URL)
+            tmp_dir = tempfile.mkdtemp()
+            input_zip.extractall(tmp_dir)
+
+            for tab in table_names:
+                setattr(
+                    self,
+                    "{}s".format(tab),
+                    Table(os.path.join(tmp_dir, "data", "{}.csv").format(tab)),
+                )
+
+
+class Table(object):
+    """Dallinger data-table object."""
+    def __init__(self, path):
+        super(Table, self).__init__()
+
+        self.odo_resource = odo.resource(path)
+        self.tablib_dataset = tablib.Dataset().load(open(path).read())
+
+    @property
+    def csv(self):
+        """Comma-separated values."""
+        return self.tablib_dataset.csv
+
+    @property
+    def dict(self):
+        """A Python dictionary."""
+        return self.tablib_dataset.dict
+
+    @property
+    def df(self):
+        """A pandas DataFrame."""
+        return odo.odo(self.odo_resource, pd.DataFrame)
+
+    @property
+    def html(self):
+        """An HTML table."""
+        return self.tablib_dataset.html
+
+    @property
+    def latex(self):
+        """A LaTeX table."""
+        return self.tablib_dataset.latex
+
+    @property
+    def list(self):
+        """A Python list."""
+        return odo.odo(self.odo_resource, list)
+
+    @property
+    def ods(self):
+        """An OpenDocument Spreadsheet."""
+        return self.tablib_dataset.ods
+
+    @property
+    def tsv(self):
+        """Tab-separated values."""
+        return self.tablib_dataset.tsv
+
+    @property
+    def xls(self):
+        """Legacy Excel spreadsheet format."""
+        return self.tablib_dataset.xls
+
+    @property
+    def xlsx(self):
+        """Modern Excel spreadsheet format."""
+        return self.tablib_dataset.xlsx
+
+    @property
+    def yaml(self):
+        """YAML."""
+        return self.tablib_dataset.yaml
