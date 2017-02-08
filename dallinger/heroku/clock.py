94d59c6982e189f29603701832c3f9757fe47283
Jordan Suchow <suchow@post.harvard.edu>
2017-02-08 01:00:57 -0500

    Add missing mode

--- dallinger/heroku/clock.py
+++ dallinger/heroku/clock.py
@@ -129,6 +129,7 @@ def check_db_for_missing_notifications():
     aws_access_key_id = config.get('aws_access_key_id')
     aws_secret_access_key = config.get('aws_secret_access_key')
     host_by_sandbox_setting = {
+        "debug": 'mechanicalturk.sandbox.amazonaws.com',
         "sandbox": 'mechanicalturk.sandbox.amazonaws.com',
         "live": 'mechanicalturk.amazonaws.com'
     }
