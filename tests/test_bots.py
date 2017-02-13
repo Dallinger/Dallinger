e430d2ae75d2712b99b9b6089618ef350907e1a6
Jordan Suchow <suchow@post.harvard.edu>
2017-02-12 19:36:51 -0500

    Import BaseBot (vs. Bot)

--- tests/test_bots.py
+++ tests/test_bots.py
@@ -3,6 +3,6 @@ class TestBots(object):
 
     def test_create_bot(self):
         """Create a bot."""
-        from dallinger.bots import Bot
-        bot = Bot("http://dallinger.io")
+        from dallinger.bots import BotBase
+        bot = BotBase("http://dallinger.io")
         assert bot
