0bc9f99e3f0ba5a563db41da7699a6369b789d55
Jordan Suchow <suchow@post.harvard.edu>
2017-02-17 17:28:31 -0500

    Remove MathJax
    MathJax, which is currently unused, causes a loading message to flash
    at the bottom left corner of every documentation page. This removes
    MathJax, thereby removing the flashing message.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,6 @@ sys.path.insert(0, src)
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon'
 ]
