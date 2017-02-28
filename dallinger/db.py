516f73287ed71fe4983b92a8dd2b61670e5b7718
David Glick <david@glicksoftware.com>
2017-02-28 13:13:27 -0800

    Run node creation/network filling view with SERIALIZABLE isolation level

--- dallinger/db.py
+++ dallinger/db.py
@@ -10,6 +10,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import DBAPIError
 
 
 logger = logging.getLogger('dallinger.db')
