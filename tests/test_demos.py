bef7b042415aa44d511bd7c516cc4638c59802d1
Jordan Suchow <suchow@post.harvard.edu>
2017-03-13 23:32:39 -0400

    Test by verifying all demos

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -1,5 +1,19 @@
 import os
+import subprocess
+
 from dallinger import db
+
+
+class TestDemos(object):
+    """Verify all the built-in demos."""
+
+    def test_verify_all_demos(self):
+        demo_paths = os.listdir("demos")
+        for demo_path in demo_paths:
+            if os.path.isdir(demo_path):
+                os.chdir(demo_path)
+                assert subprocess.check_call(["dallinger", "verify"])
+                os.chdir("..")
 
 
 class TestBartlett1932(object):
@@ -12,12 +26,12 @@ class TestBartlett1932(object):
     def setup(self):
         self._db = db.init_db(drop_all=True)
         # This is only needed for config, which loads on import
-        os.chdir('demos/bartlett1932')
+        os.chdir(os.path.join("demos", "bartlett1932"))
 
     def teardown(self):
         self._db.rollback()
         self._db.close()
-        os.chdir('../..')
+        os.chdir(os.path.join("..", ".."))
 
     def test_instantiation(self):
         demo = self._make_one()
