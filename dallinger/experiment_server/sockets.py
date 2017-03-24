f32614ea49e4d57ef8dee6700118766beaad282c
jessesnyder <jesse@rasikaconsulting.com>
2017-03-23 17:29:32 -0700

    Debugging aids

--- dallinger/experiment_server/sockets.py
+++ dallinger/experiment_server/sockets.py
@@ -39,6 +39,7 @@ class ChatBackend(object):
 
     def subscribe(self, client, channel=None):
         """Register a new client to receive messages."""
+        app.logger.debug('{} subscribing to channel {}'.format(client, channel))
         if channel is not None:
             if channel not in self.clients:
                 raise ValueError('Unknown channel: {}'.format(channel))
@@ -59,6 +60,7 @@ class ChatBackend(object):
 
         Automatically discards invalid connections.
         """
+        app.logger.debug('sending {} to client {}'.format(data, client))
         try:
             client.send(data)
         except socket.error:
