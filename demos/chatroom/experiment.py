1f8bdb746ad02073cae3d85ddcee85137b241c45
Carlos de la Guardia <cguardia@yahoo.com>
2017-04-14 11:31:03 -0500

    add chatter Bot to chatroom experiment

--- demos/chatroom/experiment.py
+++ demos/chatroom/experiment.py
@@ -1,10 +1,27 @@
 """Coordination chatroom game."""
 
+import logging
+from random import choice
+from random import expovariate
+from time import sleep
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import dallinger as dlgr
+from dallinger.bots import BotBase
 from dallinger.compat import unicode
 from dallinger.config import get_config
+from dallinger.experiments import Experiment
+
 
 config = get_config()
+logger = logging.getLogger(__file__)
 
 
 def extra_parameters():
@@ -49,3 +66,48 @@ class CoordinationChatroom(dlgr.experiment.Experiment):
     def create_node(self, participant, network):
         """Create a node for a participant."""
         return dlgr.nodes.Agent(network=network, participant=participant)
+
+
+GREETINGS = ['Hello.',
+             'How do you do.',
+             'Good day.',
+             'Anyone here?',
+             'Hi.',
+             "What's up?"]
+
+AVG_TIME_BETWEEN_MESSAGES = 2
+
+TOTAL_CHAT_TIME = 30
+
+
+class Bot(BotBase):
+    """A bot conversation demo."""
+
+    def participate(self):
+        self.chatbot = ChatBot('Dallinger Bot')
+        self.chatbot.set_trainer(ChatterBotCorpusTrainer)
+        self.chatbot.train(
+                "chatterbot.corpus.english.greetings",
+                "chatterbot.corpus.english.conversations"
+        )
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'send-message')))
+        logger.info("Entering participate method")
+        output = choice(GREETINGS)
+        self.driver.execute_script('$("#reproduction").val("%s")' % output)
+        self.driver.execute_script('$("#send-message").click()')
+        logger.info("Output: %s" % output)
+        chat_time = TOTAL_CHAT_TIME
+        while chat_time > 0:
+            waiting_time = int(expovariate(1.0/AVG_TIME_BETWEEN_MESSAGES)) + 1
+            sleep(waiting_time)
+            chat_time = chat_time - waiting_time
+            story = self.driver.find_element_by_id('story')
+            history = story.text.split('\n')
+            if history:
+                logger.info("Responding to: %s" % history[-1])
+                output = self.chatbot.get_response(history[-1])
+                self.driver.execute_script('$("#reproduction").val("%s")' % output)
+                self.driver.execute_script('$("#send-message").click()')
+                logger.info("Output: %s" % output)
+        self.driver.execute_script('leave_chatroom()')
