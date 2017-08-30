fa96f47827945b7196d8122a4df0296e254acd79
David Glick <david@glicksoftware.com>
2017-08-30 16:44:20 -0400

    Clean up Javascript.
    * Standardize formatting
    * Replace dallinger.js with dallinger2.js which puts all our JS inside a `dallinger` namespace. (dallinger.js is temporarily retained for backwards-compatibility)
    * Centralize more calls to the server.

--- demos/dlgr/demos/twentyfortyeight/static/scripts/application.js
+++ demos/dlgr/demos/twentyfortyeight/static/scripts/application.js
@@ -1,9 +1,9 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   game = new GameManager(
-      4,
-      KeyboardInputManager,
-      HTMLActuator,
-      LocalStorageManager
+    4,
+    KeyboardInputManager,
+    HTMLActuator,
+    LocalStorageManager
   );
 });
