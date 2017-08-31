c86f12a2771bdf9baea9b347ce0ef18c61cf9198
Alec Mitchell <alecpm@gmail.com>
2017-08-31 14:50:08 -0700

    Oops, remove lingering details setter from Notification route.

--- dallinger/experiment_server/experiment_server.py
+++ dallinger/experiment_server/experiment_server.py
@@ -1492,8 +1492,7 @@ def worker_function(event_type, assignment_id, participant_id, details=None):
         # save the notification to the notification table
         notif = models.Notification(
             assignment_id=assignment_id,
-            event_type=event_type,
-            details=details)
+            event_type=event_type)
         session.add(notif)
         session.commit()
 
@@ -1526,8 +1525,7 @@ def worker_function(event_type, assignment_id, participant_id, details=None):
     runner_cls = WorkerEvent.for_name(event_type)
     if runner_cls:
         runner = runner_cls(
-            participant, assignment_id, exp, session, _config(), datetime.now(),
-            details
+            participant, assignment_id, exp, session, _config(), datetime.now()
         )
         runner()
     session.commit()
