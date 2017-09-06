9f9192e04a7e5477f1d0f08b885846e9463dd337
jessesnyder <jesse@rasikaconsulting.com>
2017-09-06 15:44:47 -0700

    /question route supports pluggable Recruiters

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,7 +1,5 @@
 import pytest
-from dallinger.recruiters import CLIRecruiter
-from dallinger.recruiters import HotAirRecruiter
-from dallinger.recruiters import MTurkRecruiter
+import mock
 
 
 @pytest.mark.usefixtures('active_config')
@@ -12,22 +10,7 @@ class TestExperimentBaseClass(object):
         from dallinger.experiment import Experiment
         return Experiment()
 
-    def test_recruiter_consults_recruiter_config_value(self, exp, active_config):
-        active_config.extend({'recruiter': u'CLIRecruiter'})
-        assert isinstance(exp.recruiter, CLIRecruiter)
-
-    def test_recruiter_for_debug_mode(self, exp, active_config):
-        assert isinstance(exp.recruiter, HotAirRecruiter)
-
-    def test_recruiter_name_trumps_debug_mode(self, exp, active_config):
-        active_config.extend({'recruiter': u'CLIRecruiter'})
-        assert isinstance(exp.recruiter, CLIRecruiter)
-
-    def test_recruiter_gets_mturk_recruiter_by_default(self, exp, active_config):
-        active_config.extend({'mode': u'sandbox'})
-        assert isinstance(exp.recruiter, MTurkRecruiter)
-
-    def test_unknown_recruiter_name_raises(self, exp, active_config):
-        active_config.extend({'recruiter': u'bogus'})
-        with pytest.raises(NotImplementedError):
+    def test_recruiter_delegates(self, exp, active_config):
+        with mock.patch('dallinger.experiment.recruiters') as mock_module:
             exp.recruiter
+            mock_module.from_config.assert_called_once_with(active_config)
