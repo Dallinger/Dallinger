42489a56d91ae3ebb120329a11017f13dfbc4f41
jessesnyder <jesse@rasikaconsulting.com>
2017-09-15 11:48:20 -0700

    Test coverage and related fixes

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -9,80 +9,61 @@ class TestAgents(object):
 
     """The agent test class."""
 
-    def test_create_agent_generic(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent = nodes.Agent(network=net)
+    def test_create_agent_generic(self, a):
+        agent = a.agent()
         assert agent
 
-    def test_fitness_property(self, db_session):
-        net = models.Network()
-        agent = nodes.Agent(network=net)
+    def test_create_agent_with_participant(self, a):
+        participant = a.participant()
+        agent = a.agent(participant=participant)
+        assert agent.participant_id == participant.id
+
+    def test_fitness_property(self, a):
+        agent = a.agent()
         agent.fitness = 1.99999
         assert agent.fitness == 1.99999
 
-    def test_fitness_expression_search_match(self, db_session):
-        net = models.Network()
-        agent = nodes.Agent(network=net)
+    def test_fitness_expression_search_match(self, a):
+        agent = a.agent()
         agent.fitness = 1.99999
-        db_session.add(agent)
         results = nodes.Agent.query.filter_by(fitness=1.99999).all()
         assert len(results) == 1
         assert results[0] is agent
 
-    def test_fitness_expression_search_fail(self, db_session):
-        net = models.Network()
-        agent = nodes.Agent(network=net)
+    def test_fitness_expression_search_fail(self, a):
+        agent = a.agent()
         agent.fitness = 1.99999
-        db_session.add(agent)
         results = nodes.Agent.query.filter_by(fitness=1.9).all()
         assert len(results) == 0
 
-    def test_create_agent_generic_transmit_to_all(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent1 = nodes.Agent(network=net)
-        agent2 = nodes.Agent(network=net)
-        agent3 = nodes.Agent(network=net)
+    def test_create_agent_generic_transmit_to_all(self, a):
+        net = a.network()
+        agent1 = a.agent(network=net)
+        agent2 = a.agent(network=net)
+        agent3 = a.agent(network=net)
 
         agent1.connect(direction="to", whom=agent2)
         agent1.connect(direction="to", whom=agent3)
-        agent1.transmit(to_whom=models.Node)
+        assert agent1.transmit(to_whom=models.Node) == []
 
-    def test_fail_agent(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent = nodes.Agent(network=net)
-
-        assert agent.failed is False
-        assert agent.time_of_death is None
+    def test_fail_agent(self, a):
+        agent = a.agent()
+        assert agent.failed is False and agent.time_of_death is None
 
         agent.fail()
-        assert agent.failed is True
-        assert agent.time_of_death is not None
+        assert agent.failed is True and agent.time_of_death is not None
 
-    def test_create_replicator_agent(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent = nodes.ReplicatorAgent(network=net)
-
-        assert len(agent.infos()) is 0
-
-        info = information.Info(origin=agent, contents="foo")
-
+    def test_create_replicator_agent(self, a):
+        agent = a.replicator()
+        info = a.info(origin=agent, contents="foo")
         assert agent.infos()[0] == info
 
-    def test_agent_transmit(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-
+    def test_agent_transmit(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
-
-        info = models.Info(origin=agent1, contents="foo")
-
+        info = a.info(origin=agent1, contents="foo")
         agent1.transmit(what=agent1.infos()[0], to_whom=agent2)
         agent2.receive()
 
@@ -94,41 +75,34 @@ class TestAgents(object):
         assert transmission.origin_id == agent1.id
         assert transmission.destination_id == agent2.id
 
-    def test_agent_transmit_no_connection(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-        info = models.Info(origin=agent1, contents="foo")
+    def test_agent_transmit_no_connection(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
+        info = models.Info(origin=agent1)
         with raises(ValueError):
             agent1.transmit(what=info, to_whom=agent2)
 
-    def test_agent_transmit_invalid_info(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-
+    def test_agent_transmit_invalid_info(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
-        info = models.Info(origin=agent2, contents="foo")
+        info = a.info(origin=agent2)
 
-        with raises(ValueError):
+        with raises(ValueError) as ex_info:
             agent1.transmit(what=info, to_whom=agent2)
+            assert ex_info.match('they do not have the same origin')
 
-    def test_agent_transmit_everything_to_everyone(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-        agent3 = nodes.ReplicatorAgent(network=net)
-
+    def test_agent_transmit_everything_to_everyone(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
+        agent3 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
         agent1.connect(direction="to", whom=agent3)
-        info = models.Info(origin=agent1, contents="foo")
-
+        info = a.info(origin=agent1)
         agent1.transmit(what=models.Info, to_whom=nodes.Agent)
-
         agent2.receive()
         agent3.receive()
 
@@ -139,22 +113,19 @@ class TestAgents(object):
         transmissions = info.transmissions()
         assert len(transmissions) == 2
 
-    def test_transmit_selector_default(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        # Create a network of two biological nodes.
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-
+    def test_transmit_selector_default(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
 
-        information.Meme(origin=agent1, contents="foo")
-        information.Gene(origin=agent1, contents="bar")
+        a.meme(origin=agent1, contents="foo")
+        a.gene(origin=agent1, contents="bar")
 
         assert len(agent1.infos(type=Gene)) == 1
-        assert len(agent1.infos(type=Gene)) == 1
+        assert len(agent1.infos(type=Meme)) == 1
         assert len(agent2.infos(type=Gene)) == 0
-        assert len(agent2.infos(type=Gene)) == 0
+        assert len(agent2.infos(type=Meme)) == 0
 
         # Transmit from agent 1 to 2.
         agent1.transmit(to_whom=agent2)
@@ -166,22 +137,18 @@ class TestAgents(object):
         assert "foo" == agent2.infos(type=Meme)[0].contents
         assert "bar" == agent2.infos(type=Gene)[0].contents
 
-    def test_transmit_selector_specific_info(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-        # Create a network of two biological nodes.
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-
+    def test_transmit_selector_specific_info(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
-
-        information.Meme(origin=agent1, contents="foo")
-        gene = information.Gene(origin=agent1, contents="bar")
+        a.meme(origin=agent1)
+        gene = a.gene(origin=agent1, contents="foo")
 
         assert len(agent1.infos(type=Gene)) == 1
-        assert len(agent1.infos(type=Gene)) == 1
+        assert len(agent1.infos(type=Meme)) == 1
         assert len(agent2.infos(type=Gene)) == 0
-        assert len(agent2.infos(type=Gene)) == 0
+        assert len(agent2.infos(type=Meme)) == 0
 
         # Transmit from agent 1 to 2.
         agent1.transmit(what=gene, to_whom=agent2)
@@ -191,22 +158,18 @@ class TestAgents(object):
 
         # Make sure that Agent 2 has a blank memome and the right gene.
         assert not agent2.infos(type=Meme)
-        assert "bar" == agent2.infos(type=Gene)[0].contents
+        assert "foo" == agent2.infos(type=Gene)[0].contents
 
-    def test_transmit_selector_all_of_type(self, db_session):
-        net = models.Network()
-        db_session.add(net)
-
-        # Create a network of two biological nodes.
-        agent1 = nodes.ReplicatorAgent(network=net)
-        agent2 = nodes.ReplicatorAgent(network=net)
-
+    def test_transmit_selector_all_of_type(self, a):
+        net = a.network()
+        agent1 = a.replicator(network=net)
+        agent2 = a.replicator(network=net)
         agent1.connect(direction="to", whom=agent2)
 
-        information.Meme(origin=agent1, contents="foo1")
-        information.Meme(origin=agent1, contents="foo2")
-        information.Meme(origin=agent1, contents="foo3")
-        information.Gene(origin=agent1, contents="bar")
+        a.meme(origin=agent1)
+        a.meme(origin=agent1)
+        a.meme(origin=agent1)
+        a.gene(origin=agent1)
 
         assert len(agent1.infos(type=Meme)) == 3
         assert len(agent2.infos(type=Meme)) == 0
