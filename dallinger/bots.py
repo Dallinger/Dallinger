498f801ae01cd0401299d7dce83a187bdfd1969e
Carlos de la Guardia <cguardia@yahoo.com>
2017-12-08 10:29:25 -0600

    add missing default parameter

--- dallinger/bots.py
+++ dallinger/bots.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__file__)
 class BotBase(object):
     """A base class for Bots that works with the built-in demos."""
 
-    def __init__(self, URL, assignment_id='', worker_id=''):
+    def __init__(self, URL, assignment_id='', worker_id='', participant_id=''):
         logger.info("Creating bot with URL: %s." % URL)
         self.URL = URL
 
