af22c681fa0df48f99d57cc51c5ba628b9d8d8e2
omonida <manganess512@gmail.com>
2018-01-10 08:42:08 -0800

    Interface with daytime text entry and non-functional voting

--- demos/mafia/experiment.py
+++ demos/mafia/experiment.py
@@ -4,14 +4,17 @@ import json
 import random
 import time
 
-from sqlalchemy import String
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import Boolean
 from sqlalchemy.sql.expression import cast
 
 import dallinger as dlgr
-from dallinger.models import Node, Network
-from dallinger.networks import Empty
-from dallinger.networks import FullyConnected
+from dallinger.models import Node, Network, timenow
+# from dallinger.networks import Empty
+# from dallinger.networks import FullyConnected
+# from dallinger.networks import MafiaNetwork
 from dallinger.nodes import Source
+# from dallinger.command_line import log
 
 
 class CoordinationChatroom(dlgr.experiments.Experiment):
@@ -22,18 +25,20 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         super(CoordinationChatroom, self).__init__(session)
         import models
         self.models = models
-        
+
         self.experiment_repeats = 1
-        self.num_participants = 4 #55 #55 #140 below
-        self.initial_recruitment_size = self.num_participants #self.num_participants * 2 #note: can't do *2.5 here, won't run even if the end result is an integer
+        self.num_participants = 3 #2 #55 #55 #140 below
+        self.initial_recruitment_size = self.num_participants # * 2 # Note: can't do *2.5 here, won't run even if the end result is an integer
         self.quorum = self.num_participants
-        #self.setup()
         if session:
             self.setup()
         self.num_mafia = 2
         self.mafia = random.sample(range(self.num_participants), self.num_mafia)
-        # self.networks()[0].setup_nighttime()
-        self.round_duration = 120
+        # self.round_duration = 120
+        self.round_duration = 1
+        self.start_time = timenow()
+        # print(self.start_time)
+        # self.start_time = time.time()
 
     def setup(self):
         """Setup the networks.
@@ -50,10 +55,10 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
 
     def create_network(self):
         """Create a new network by reading the configuration file."""
-        #return Empty(max_size=self.num_participants + 1)  # add a Source
-        print('HELLO')
-        return FullyConnected(max_size=self.num_participants + 1)  # add a Source
-        #return self.models.MafiaNetwork(max_size=self.num_participants + 1)  # add a Source
+
+        mafia_network = self.models.MafiaNetwork(max_size=self.num_participants + 1)  # add a Source
+        # mafia_network.daytime = False
+        return mafia_network
 
 
     def bonus(self, participant):
@@ -70,6 +75,7 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         network.add_node(node)
         source = network.nodes(type=Source)[0]  # find the source in the network
         source.connect(direction="to", whom=node)  # link up the source to the new node
+        # source.connect(direction="both", whom=node)  # link up the source to the new node
         source.transmit(to_whom=node)  # in networks.py code, transmit info to the new node
         node.receive()  # new node receives everything
 
@@ -95,23 +101,35 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         """Run when a request to create an info is complete."""
 
         # Figure out if it's night or daytime based on current clocktime
+        # elapsed_time = self.start_time
         # elapsed_time = time.time() - self.start_time
-        # # round_duration = 120
-        #
-        # # Get the current status of the network (night vs. day) by querying
-        # # for the network and then checking its .daytime property.
-        # net = Network.query.filter_by(network_id=node.network_id)
-        # daytime = net.daytime
-        #
-        # # If it's night but should be day, then call setup_daytime()
-        # if (not net.daytime and (elapsed_time / self.round_duration) % 2 == 1):
-        #     # setup_daytime()
+        elapsed_time = timenow() - self.start_time
+        # log("elapsed_time")
+        # self.round_duration = elapsed_time - self.round_duration
+
+        # Get the current status of the network (night vs. day) by querying
+        # for the network and then checking its .daytime property.
+        net = Network.query.filter_by(id=node.network_id).one()
+        # net = node.network
+        daytime = net.daytime
+
+        # If it's night but should be day, then call setup_daytime()
+        # if (not daytime) and ((elapsed_time.total_seconds() // self.round_duration) % 2 == 1):
+        # if (not daytime) and (elapsed_time.total_seconds() >= self.round_duration):
         #     net.setup_daytime()
-        #
+
         # # If it's day but should be night, then call setup_nighttime()
-        # if (net.daytime and (elapsed_time / self.round_duration) % 2 == 0):
-        # # if (net.daytime):
-        #     # setup_nighttime()
+        # if daytime and ((elapsed_time.total_seconds() // self.round_duration) % 2 == 0):
+        # # if (daytime):
+        #     net.setup_nighttime()
+
+        if not daytime:
+            net.setup_daytime()
+
+        # if not votetime:
+        #     net.setup_votetime()
+        #
+        # if net.daytime:
         #     net.setup_nighttime()
 
         # Proceed with normal info post request
@@ -124,19 +142,19 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         # Check how many mafia members there are.
         # num_mafioso = Nodes.query.filter....count()
         # num_mafioso = Node.query.filter_by(__mapper_args__['polymorphic_identity']="mafioso").count()
-        # num_mafioso = Node.query.filter_by(type="mafioso").count()
-        node_num = Node.query.filter_by().count()
         # If there aren't enough, create another:
         # SAMEE is participant an integer? is it sequential?
-        # if num_mafioso < 2:
-        if node_num + 1 == self.quorum:
-            self.start_time = time.time()
-        if node_num in self.mafia:
+        # if Node.query.filter_by().count() == self.quorum:
+        # self.start_time = time.time()
+        # self.start_time = timenow()
+        num_mafioso = Node.query.filter_by(type="mafioso").count()
+        if num_mafioso < self.num_mafia:
+        # node_num = Node.query.filter_by().count() - 1
+        # if node_num in self.mafia:
             return self.models.Mafioso(network=network, participant=participant)
         else:
             return self.models.Bystander(network=network, participant=participant)
         #return dlgr.nodes.Agent(network=network, participant=participant)
-
 
 
 class FreeRecallListSource(Source):
@@ -152,7 +170,7 @@ class FreeRecallListSource(Source):
         """
 
         #CODE FOR INDIVIDUAL EXPTS
-        #(samples 60 words from the big wordlist for each participant) 
+        #(samples 60 words from the big wordlist for each participant)
         # wordlist = "groupwordlist.md"
         # with open("static/stimuli/{}".format(wordlist), "r") as f:
         #    wordlist = f.read().splitlines()
@@ -161,13 +179,13 @@ class FreeRecallListSource(Source):
 
 
         # CODE FOR GROUP EXPTS
-        # (has one word list for the experiment 
+        # (has one word list for the experiment
         # (draw 60 words from "groupwordlist.md") then
         # reshuffles the words within each participant
 
         ### read in UUID
         exptfilename = "experiment_id.txt"
-        exptfile = open(exptfilename, "r") 
+        exptfile = open(exptfilename, "r")
         UUID = exptfile.read() # get UUID of the experiment
 
         wordlist = "groupwordlist.md"
@@ -175,13 +193,13 @@ class FreeRecallListSource(Source):
 
         	### get a wordlist for the expt
         	# reads in the file with a big list of words
-        	wordlist = f.read().splitlines() 
-        	# use the UUID (unique to each expt) as a seed for 
+        	wordlist = f.read().splitlines()
+        	# use the UUID (unique to each expt) as a seed for
         	# the pseudorandom number generator.
-        	# the random sample will be the same for everyone within an 
-        	# experiment but different across experiments b/c 
+        	# the random sample will be the same for everyone within an
+        	# experiment but different across experiments b/c
         	# they have different UUIDs.
-        	random.seed(UUID) 
+        	random.seed(UUID)
         	# sample 60 words from large word list without replacement
         	expt_wordlist = random.sample(wordlist,1)
 
@@ -189,13 +207,13 @@ class FreeRecallListSource(Source):
         	random.seed() # an actually random seed
         	random.shuffle(expt_wordlist)
         	return json.dumps(expt_wordlist)
+        # return json.dumps(self.network.nodes())
+        # return json.dumps(Network.query.filter_by(id=self.network_id).one().nodes())
 
 
-        	
-
-        # OLD: 
+        # OLD:
         # shuffles all words
-        #wordlist = "60words.md" 
+        #wordlist = "60words.md"
         #with open("static/stimuli/{}".format(wordlist), "r") as f:
         #    wordlist = f.read().splitlines()
         #    return json.dumps(random.sample(wordlist,60))
