9af54513ba12f6ce4352edcdc58d79a0975ad6d9
omonida <manganess512@gmail.com>
2018-01-11 21:26:04 -0800

    Fixed phase switching and voting

--- demos/mafia/experiment.py
+++ demos/mafia/experiment.py
@@ -1,4 +1,4 @@
-"""Coordination chatroom game."""
+"""Mafia game."""
 
 import json
 import random
@@ -9,20 +9,24 @@ from sqlalchemy import Boolean
 from sqlalchemy.sql.expression import cast
 
 import dallinger as dlgr
+from dallinger import db
 from dallinger.models import Node, Network, timenow
 # from dallinger.networks import Empty
 # from dallinger.networks import FullyConnected
 # from dallinger.networks import MafiaNetwork
 from dallinger.nodes import Source
 # from dallinger.command_line import log
+from flask import Blueprint, Response
+from faker import Faker
+fake = Faker()
 
 
-class CoordinationChatroom(dlgr.experiments.Experiment):
+class MafiaExperiment(dlgr.experiments.Experiment):
     """Define the structure of the experiment."""
 
     def __init__(self, session):
         """Initialize the experiment."""
-        super(CoordinationChatroom, self).__init__(session)
+        super(MafiaExperiment, self).__init__(session)
         import models
         self.models = models
 
@@ -32,11 +36,11 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         self.quorum = self.num_participants
         if session:
             self.setup()
-        self.num_mafia = 2
-        self.mafia = random.sample(range(self.num_participants), self.num_mafia)
+        self.num_mafia = 1
+        # self.mafia = random.sample(range(self.num_participants), self.num_mafia)
         # self.round_duration = 120
-        self.round_duration = 1
-        self.start_time = timenow()
+        self.round_duration = 0
+        # self.start_time = timenow()
         # print(self.start_time)
         # self.start_time = time.time()
 
@@ -49,7 +53,7 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         source to each network.
         """
         if not self.networks():
-            super(CoordinationChatroom, self).setup()
+            super(MafiaExperiment, self).setup()
             for net in self.networks():
                 FreeRecallListSource(network=net)
 
@@ -57,7 +61,7 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         """Create a new network by reading the configuration file."""
 
         mafia_network = self.models.MafiaNetwork(max_size=self.num_participants + 1)  # add a Source
-        # mafia_network.daytime = False
+        mafia_network.daytime = 'False'
         return mafia_network
 
 
@@ -100,39 +104,41 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
     def info_post_request(self, node, info):
         """Run when a request to create an info is complete."""
 
-        # Figure out if it's night or daytime based on current clocktime
-        # elapsed_time = self.start_time
-        # elapsed_time = time.time() - self.start_time
-        elapsed_time = timenow() - self.start_time
-        # log("elapsed_time")
-        # self.round_duration = elapsed_time - self.round_duration
-
-        # Get the current status of the network (night vs. day) by querying
-        # for the network and then checking its .daytime property.
-        net = Network.query.filter_by(id=node.network_id).one()
-        # net = node.network
-        daytime = net.daytime
-
-        # If it's night but should be day, then call setup_daytime()
+        # # Figure out if it's night or daytime based on current clocktime
+        # # elapsed_time = self.start_time
+        # # elapsed_time = time.time() - self.start_time
+        # # elapsed_time = timenow() - self.start_time
+        # elapsed_time = timenow() - Node.query.order_by('creation_time').first().creation_time
+        # # elapsed_time = timenow() - Participant.query.filter().order_by('creation_time').last() # creation time of participant
+        # # log("elapsed_time")
+        # # self.round_duration = elapsed_time - self.round_duration
+        #
+        # # Get the current status of the network (night vs. day) by querying
+        # # for the network and then checking its .daytime property.
+        # net = Network.query.filter_by(id=node.network_id).one()
+        # # net = node.network
+        # daytime = net.daytime
+        #
+        # # If it's night but should be day, then call setup_daytime()
         # if (not daytime) and ((elapsed_time.total_seconds() // self.round_duration) % 2 == 1):
-        # if (not daytime) and (elapsed_time.total_seconds() >= self.round_duration):
+        # # if (not daytime) and (elapsed_time.total_seconds() >= self.round_duration):
         #     net.setup_daytime()
-
-        # # If it's day but should be night, then call setup_nighttime()
+        #
+        # # # If it's day but should be night, then call setup_nighttime()
         # if daytime and ((elapsed_time.total_seconds() // self.round_duration) % 2 == 0):
         # # if (daytime):
         #     net.setup_nighttime()
-
-        if not daytime:
-            net.setup_daytime()
-
-        # if not votetime:
-        #     net.setup_votetime()
         #
-        # if net.daytime:
-        #     net.setup_nighttime()
-
-        # Proceed with normal info post request
+        # # if not daytime:
+        # #     net.setup_daytime()
+        #
+        # # if not votetime:
+        # #     net.setup_votetime()
+        # #
+        # # if net.daytime:
+        # #     net.setup_nighttime()
+        #
+        # # Proceed with normal info post request
         for agent in node.neighbors():
             node.transmit(what=info, to_whom=agent)
 
@@ -149,13 +155,82 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         # self.start_time = timenow()
         num_mafioso = Node.query.filter_by(type="mafioso").count()
         if num_mafioso < self.num_mafia:
-        # node_num = Node.query.filter_by().count() - 1
-        # if node_num in self.mafia:
-            return self.models.Mafioso(network=network, participant=participant)
+        # # num_nodes = Node.query.count() - 1
+        # if num_mafioso < self.num_mafia and random.random() < self.num_mafia / self.num_participants:
+        # # node_num = Node.query.count() - 1
+        # # if node_num in self.mafia:
+            mafioso = self.models.Mafioso(network=network, participant=participant)
+            mafioso.fake_name = str(fake.name())
+            mafioso.alive = 'True'
+            return mafioso
+        # elif self.num_participants - num_nodes == self.num_mafia - num_mafioso:
+        #     return self.models.Mafioso(network=network, participant=participant)
         else:
-            return self.models.Bystander(network=network, participant=participant)
+            bystander = self.models.Bystander(network=network, participant=participant)
+            bystander.fake_name = str(fake.name())
+            bystander.alive = 'True'
+            return bystander
         #return dlgr.nodes.Agent(network=network, participant=participant)
 
+extra_routes = Blueprint(
+    'extra_routes',
+    __name__,
+    template_folder='templates',
+    static_folder='static')
+
+
+@extra_routes.route("/phase", methods=["GET"])
+def phase():
+    try:
+        exp = MafiaExperiment(db.session)
+        nodes = Node.query.order_by('creation_time').all()
+        node = nodes[-1]
+        elapsed_time = timenow() - node.creation_time
+        net = Network.query.filter_by(id=node.network_id).one()
+        daytime = (net.daytime == 'True')
+        round_duration = 30
+        name = elapsed_time.total_seconds()
+        end = False
+        winner = None
+
+        # If it's night but should be day, then call setup_daytime()
+        if not daytime and ((elapsed_time.total_seconds() // round_duration) % 2 == 1):
+            name = net.setup_daytime()
+
+        # If it's day but should be night, then call setup_nighttime()
+        if daytime and ((elapsed_time.total_seconds() // round_duration) % 2 == 0):
+            name, end, winner = net.setup_nighttime()
+
+        exp.save()
+
+        return Response(
+            # response=net.daytime,
+            # response=str(name),
+            response=json.dumps({ "name": name, "end": end, "winner": winner }),
+            status=200,
+            mimetype='application/json')
+    except:
+        return Response(
+            status=403,
+            mimetype='application/json')
+
+# @extra_routes.route("/name/<int:node_id>", methods=["GET"])
+# def name(node_id):
+#     try:
+#         exp = MafiaExperiment(db.session)
+#         node = Node.query.filter_by(id=node_id).one()
+#         fake_name = node.fake_name
+#
+#         exp.save()
+#
+#         return Response(
+#             response=str(fake_name),
+#             status=200,
+#             mimetype='application/json')
+#     except:
+#         return Response(
+#             status=403,
+#             mimetype='application/json')
 
 class FreeRecallListSource(Source):
     """A Source that reads in a random list from a file and transmits it."""
