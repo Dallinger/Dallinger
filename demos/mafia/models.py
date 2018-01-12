809484b62ace006af86d740dd17e282e7c62014c
omonida <manganess512@gmail.com>
2018-01-11 21:26:04 -0800

    Fixed phase switching and voting

--- demos/mafia/models.py
+++ demos/mafia/models.py
@@ -4,23 +4,60 @@
 # import random
 
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy import Boolean
+from sqlalchemy import Boolean, String
 from sqlalchemy.sql.expression import cast
 
 # from dallinger.information import State
 # from dallinger.models import Info
-from dallinger.models import Node, Network
+from dallinger.models import Node, Network, Info, timenow
 from dallinger.nodes import Source
 
-class Mafioso(Node):
-    """Member of the mafia."""
-
-    __mapper_args__= {"polymorphic_identity":"mafioso"}
 
 class Bystander(Node):
     """Bystander"""
 
     __mapper_args__= {"polymorphic_identity":"bystander"}
+
+    @hybrid_property
+    def fake_name(self):
+        """Convert property1 to fake name."""
+        try:
+            return self.property1
+        except TypeError:
+            return None
+
+    @fake_name.setter
+    def fake_name(self, name):
+        """Make name settable."""
+        self.property1 = name
+
+    @fake_name.expression
+    def fake_name(self):
+        """Make name queryable."""
+        return self.property1
+
+    @hybrid_property
+    def alive(self):
+        """Convert property2 to alive."""
+        try:
+            return self.property2
+        except TypeError:
+            return None
+
+    @alive.setter
+    def alive(self, is_alive):
+        """Make alive settable."""
+        self.property2 = is_alive
+
+    @alive.expression
+    def alive(self):
+        """Make alive queryable."""
+        return self.property2
+
+class Mafioso(Bystander):
+    """Member of the mafia."""
+
+    __mapper_args__= {"polymorphic_identity":"mafioso"}
 
 
 class MafiaNetwork(Network):
@@ -44,59 +81,80 @@ class MafiaNetwork(Network):
     def daytime(self):
         """Convert property1 to daytime."""
         try:
-            return bool(self.property1)
+            return self.property1
         except TypeError:
             return None
 
     @daytime.setter
     def daytime(self, is_daytime):
         """Make time settable."""
-        self.property1 = repr(is_daytime)
+        self.property1 = is_daytime
 
     @daytime.expression
     def daytime(self):
         """Make time queryable."""
-        return cast(self.property1, Boolean)
-
-    # @hybrid_property
-    # def votetime(self):
-    #     """Convert property2 to votetime."""
-    #     try:
-    #         return bool(self.property2)
-    #     except TypeError:
-    #         return None
-    #
-    # @votetime.setter
-    # def votetime(self, is_votetime):
-    #     """Make time settable."""
-    #     self.property2 = repr(is_votetime)
-    #
-    # @votetime.expression
-    # def votetime(self):
-    #     """Make time queryable."""
-    #     return cast(self.property2, Boolean)
+        return self.property1
 
     def fail_bystander_vectors(self):
-        mafiosi = self.nodes(type=Mafioso)
+        # mafiosi = self.nodes(type=Mafioso)
+        mafiosi = Node.query.filter_by(network_id=self.id, property2='True', type='mafioso').all()
         for v in self.vectors():
             if not isinstance(v.origin, Source) and not (v.origin in mafiosi and v.destination in mafiosi):
                 v.fail()
 
+    def vote(self, nodes):
+        votes = {}
+        for node in nodes:
+            infos = Info.query.filter_by(origin_id=node.id).order_by('creation_time')
+            vote = infos[-1].contents.split(': ')[1]
+            if vote in votes:
+                votes[vote] += 1
+            else:
+                votes[vote] = 1
+        k = list(votes.keys())
+        v = list(votes.values())
+        victim_name = k[v.index(max(v))]
+        victim_node = Node.query.filter_by(property1=victim_name).one()
+        victim_node.alive = 'False'
+        for v in victim_node.vectors():
+            v.fail()
+        for i in victim_node.infos():
+            i.fail()
+        for t in victim_node.transmissions(direction="all"):
+            t.fail()
+        for t in victim_node.transformations():
+            t.fail()
+        return victim_name
+
     def setup_daytime(self):
-        self.daytime = True
-        nodes = [n for n in self.nodes() if not isinstance(n, Source)]
+        # mafiosi = self.nodes(type=Mafioso)
+        mafiosi = Node.query.filter_by(network_id=self.id, property2='True', type='mafioso').all()
+        victim_name = self.vote(mafiosi)
+        self.daytime = 'True'
+        # nodes = [n for n in self.nodes() if not isinstance(n, Source)]
+        nodes = [n for n in Node.query.filter_by(network_id=self.id, property2='True').all() if not isinstance(n, Source)]
         for n in nodes:
             for m in nodes:
                 if n != m:
                     n.connect(whom=m, direction="to")
+        # return 'victim_name'
+        return victim_name
 
     def setup_nighttime(self):
-        self.daytime = False
+        # nodes = self.nodes()
+        nodes = Node.query.filter_by(network_id=self.id, property2='True').all()
+        victim_name = self.vote(nodes)
+        mafiosi = Node.query.filter_by(network_id=self.id, property2='True', type='mafioso').all()
+        game_end = False
+        winner = None
+        if len(mafiosi) >= (len(nodes) - 1) / 2:
+            game_end = True
+            winner = 'mafia'
+            return victim_name, game_end, winner
+        if len(mafioso) == 0:
+            game_end = True
+            winner = 'townspeople'
+            return victim_name, game_end, winner
+        self.daytime = 'False'
         self.fail_bystander_vectors()
-
-    # def setup_votetime(self):
-    #     self.votetime = True
-    #     source = self.nodes(type=Source)[0]
-    #     nodes = [n for n in self.nodes() if not isinstance(n, Source)]
-    #     for n in nodes:
-    #         source.trasmit(what=nodes, to_whom=n,)
+        return victim_name, game_end, winner
