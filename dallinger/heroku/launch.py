0511063582a3800258477903b395b62512aec474
David Glick <david@glicksoftware.com>
2018-04-16 18:28:00 -0400

    Try patching here

--- dallinger/heroku/launch.py
+++ dallinger/heroku/launch.py
@@ -1,5 +1,9 @@
 """Launch the experiment server."""
 
+# Make sure gevent patches are applied early
+import gevent.monkey
+gevent.monkey.patch_all()
+
 from dallinger.experiment_server.gunicorn import launch
 
 
