c63e6c59d10a77a23f5382454433a8ff3e9de6d1
David Glick <david@glicksoftware.com>
2018-04-16 18:45:05 -0400

    Avoid flake8 warning about import order

--- dallinger/heroku/launch.py
+++ dallinger/heroku/launch.py
@@ -1,11 +1,9 @@
 """Launch the experiment server."""
 
-# Make sure gevent patches are applied early
-import gevent.monkey
-gevent.monkey.patch_all()
-
-from dallinger.experiment_server.gunicorn import launch
-
-
 if __name__ == '__main__':
+    # Make sure gevent patches are applied early
+    import gevent.monkey
+    gevent.monkey.patch_all()
+
+    from dallinger.experiment_server.gunicorn import launch
     launch()
