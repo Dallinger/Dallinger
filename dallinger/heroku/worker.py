6170a348a62bf976ca4c88744787d1412599cd68
David Glick <david@glicksoftware.com>
2018-05-01 10:04:55 -0400

    skip coverage for queue worker

--- dallinger/heroku/worker.py
+++ dallinger/heroku/worker.py
@@ -9,7 +9,7 @@ listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: nocover
     from rq import (
         Queue,
         Connection
