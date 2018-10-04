a28217982b29d3e5a9b5321e5cb6c6c3d8233b5c
Jesse Snyder <jesse@rasikaconsulting.com>
2018-10-04 13:07:52 -0700

    Committing sketch of how role reshuffle should basically look

--- dallinger/heroku/clock.py
+++ dallinger/heroku/clock.py
@@ -1,21 +1,16 @@
 """A clock process."""
 
 from datetime import datetime
-import json
-
 from apscheduler.schedulers.blocking import BlockingScheduler
-import requests
 
 import dallinger
 from dallinger import db
 from dallinger.models import Participant
 from dallinger.mturk import MTurkService
-from dallinger.mturk import MTurkServiceException
 from dallinger.heroku.messages import HITSummary
 from dallinger.heroku.messages import get_messenger
 from dallinger.heroku.messages import MessengerError
-from dallinger.heroku import tools as heroku_tools
-from dallinger.recruiters import BotRecruiter
+from dallinger import recruiters
 
 
 # Import the experiment.
@@ -30,110 +25,41 @@ def run_check(config, mturk, participants, session, reference_time):
 
     # get experiment duration in seconds
     duration_seconds = config.get('duration') * 60.0 * 60.0
-    notification_url = config.get('notification_url')
-    dallinger_uid = config.get('id')
 
+    # Track HITs somehow... TBD.
+    hits_to_be_cancelled = set()
     # for each participant, if they've been active for longer than the
     # experiment duration + 5 minutes, we take action.
     for p in participants:
         time_active = (reference_time - p.creation_time).total_seconds()
 
         if time_active > (duration_seconds + 120):
-            print("Error: participant {} with status {} has been playing for too "
-                  "long and no notification has arrived - "
-                  "running emergency code".format(p.id, p.status))
+            print(
+                "Error: participant {} with status {} has been playing for too "
+                "long - checking in with their recruiter.".format(p.id, p.status)
+            )
 
-            # get their assignment
-            assignment_id = p.assignment_id
-
-            # First see if we have a bot participant
-            if (p.recruiter_id == BotRecruiter.nickname or
-                    p.recruiter_id.startswith('bots:')):
-                # Bot somehow did not finish (phantomjs?). Just get rid of it.
-                p.status = "rejected"
-                session.commit()
-                return
-
-            # ask amazon for the status of the assignment
-            try:
-                assignment = mturk.get_assignment(assignment_id)
-                status = assignment['status']
-            except Exception:
-                status = None
-            print("assignment status from AWS is {}".format(status))
-            hit_id = p.hit_id
+            recruiter = recruiters.by_name(p.recruiter_id)
             summary = HITSummary(
-                assignment_id=assignment_id,
+                assignment_id=p.assignment_id,
                 duration=duration_seconds,
                 time_active=time_active,
                 app_id=config.get('id', 'unknown'),
                 when=reference_time,
             )
-            messenger = get_messenger(summary, config)
 
-            if status == "Approved":
-                # if its been approved, set the status accordingly
-                print("status set to approved")
-                p.status = "approved"
-                session.commit()
-            elif status == "Rejected":
-                print("status set to rejected")
-                # if its been rejected, set the status accordingly
-                p.status = "rejected"
-                session.commit()
-            elif status == "Submitted":
-                # if it has been submitted then resend a submitted notification
-                args = {
-                    'Event.1.EventType': 'AssignmentSubmitted',
-                    'Event.1.AssignmentId': assignment_id
-                }
-                requests.post(notification_url, data=args)
+            recruiter.notify_duration_exceeded(p, summary)
 
-                # message the researcher:
-                messenger.send_resubmitted_msg()
-
-                print("Error - submitted notification for participant {} missed. "
-                      "Database automatically corrected, but proceed with caution."
-                      .format(p.id))
-            else:
-                # if it has not been submitted shut everything down
-                # first turn off autorecruit
-                heroku_app = heroku_tools.HerokuApp(dallinger_uid)
-                args = json.dumps({"auto_recruit": "false"})
-                headers = heroku_tools.request_headers(config.get("heroku_auth_token"))
-                requests.patch(
-                    heroku_app.config_url,
-                    data=args,
-                    headers=headers,
-                )
-
-                # Attempt to force-expire the hit via boto. It's possible
-                # that the HIT won't exist, either because we didn't use the
-                # MTurkRecruiter, or because the HIT has been deleted manually.
-                try:
-                    mturk.expire_hit(hit_id)
-                except MTurkServiceException as ex:
-                    print(ex)
-
-                # message the researcher
-                try:
-                    messenger.send_hit_cancelled_msg()
-                except MessengerError as ex:
-                    print(ex)
-
-                # send a notificationmissing notification
-                args = {
-                    'Event.1.EventType': 'NotificationMissing',
-                    'Event.1.AssignmentId': assignment_id
-                }
-
-                print("Sending notification to {}".format(notification_url))
-                requests.post(notification_url, data=args)
-
-                print("Error - abandoned/returned notification for participant {} missed. "
-                      "Experiment shut down. Please check database and then manually "
-                      "resume experiment."
-                      .format(p.id))
+    for hit in hits_to_be_cancelled:
+        # Do something to cancel HITs
+        recruiter = recruiter.by_name(hit['recruiter_id'])
+        recruiter.close_recruitment(hit['id'])
+        recruiter.terminate_hit(hit['id'])
+        messenger = get_messenger(hit['summary'], config)
+        try:
+            messenger.send_hit_cancelled_msg()
+        except MessengerError as ex:
+            print(ex)
 
 
 @scheduler.scheduled_job('interval', minutes=0.5)
