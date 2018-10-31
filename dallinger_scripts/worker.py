b672ce9ee076e5989317a87dfed489a901a21e64
Alec Mitchell <alecpm@gmail.com>
2018-10-31 01:20:12 -0700

    Use local gevent patched redis connection in worker.

--- dallinger_scripts/worker.py
+++ dallinger_scripts/worker.py
@@ -1,24 +1,23 @@
 """Heroku web worker."""
-# Make sure gevent patches are applied early.
-import os
-
 listen = ['high', 'default', 'low']
 
 
 def main():
     import gevent.monkey
     gevent.monkey.patch_all()
+    from gevent.queue import LifoQueue
 
     # These imports are inside the __main__ block
     # to make sure that we only import from rq_gevent_worker
     # (which has the side effect of applying gevent monkey patches)
     # in the worker process. This way other processes can import the
     # redis connection without that side effect.
+    import os
+    from redis import BlockingConnectionPool, StrictRedis
     from rq import (
         Queue,
         Connection
     )
-    from dallinger.db import redis_conn
     from dallinger.heroku.rq_gevent_worker import GeventWorker as Worker
 
     from dallinger.config import initialize_experiment_package
@@ -26,6 +25,10 @@ def main():
 
     import logging
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+    redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+    redis_pool = BlockingConnectionPool.from_url(redis_url,
+                                                 queue_class=LifoQueue)
+    redis_conn = StrictRedis(connection_pool=redis_pool)
 
     with Connection(redis_conn):
         worker = Worker(list(map(Queue, listen)))
