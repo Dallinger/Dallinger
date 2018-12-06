b2dd9c7351266c63195766afdcd43b495c4b6040
witekdev <witekdev@gmail.com>
2018-12-06 16:37:50 -0500

    Fix snake, concentration and 2048 demos non-terminating after demo completes

--- demos/dlgr/demos/snake/experiment.py
+++ demos/dlgr/demos/snake/experiment.py
@@ -1,5 +1,7 @@
 """The game Snake."""
 import dallinger as dlgr
+from dallinger.experiment import Experiment
+from dallinger.networks import Empty
 
 config = dlgr.config.get_config()
 
@@ -8,7 +10,7 @@ def extra_parameters():
     config.register('n', int)
 
 
-class SnakeGame(dlgr.experiment.Experiment):
+class SnakeGame(Experiment):
     """Define the structure of the experiment."""
 
     def __init__(self, session=None):
@@ -18,3 +20,7 @@ class SnakeGame(dlgr.experiment.Experiment):
         self.initial_recruitment_size = config.get("n")
         if session:
             self.setup()
+
+    def create_network(self):
+        """Return a new network."""
+        return Empty(max_size=self.initial_recruitment_size)
