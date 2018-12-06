48540359502b41822d72d95362e34d5c4e1334f0
witekdev <witekdev@gmail.com>
2018-12-06 16:56:22 -0500

    Fix voxpopuli demo and update some config descriptions of demos if inaccurate.

--- demos/dlgr/demos/vox_populi/experiment.py
+++ demos/dlgr/demos/vox_populi/experiment.py
@@ -15,10 +15,10 @@ class VoxPopuli(Experiment):
         """
         super(VoxPopuli, self).__init__(session)
         self.experiment_repeats = 1
-        self.initial_recruitment = 20
+        self.initial_recruitment_size = 2
         if session:
             self.setup()
 
     def create_network(self):
         """Return a new network."""
-        return Empty(max_size=20)
+        return Empty(max_size=self.initial_recruitment_size)
