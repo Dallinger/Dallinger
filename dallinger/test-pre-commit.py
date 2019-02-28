64c7b60ecc30a64042cc20e929262c5e7aa280ed
vlall <vishal.h.lall@gmail.com>
2019-02-28 14:27:12 -0800

    Test of hook 3

--- dallinger/test-pre-commit.py
+++ dallinger/test-pre-commit.py
@@ -1,12 +1,18 @@
 import os, json
+import os
 
-print("A reallllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllly long line")
+print(
+    "A reallllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllly long line"
+)
 
-print("spacing on this file is also not great !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print(
+    "spacing on this file is also not great !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+)
 
-print("""what do we expect the precommit hook to
-do""")
-x = 2+3
-if x>2:
-	print("test")
-
+print(
+    """what do we expect the precommit hook to
+do"""
+)
+x = 2 + 3
+if x > 2:
+    print("test")
