1d7397ef342c3889f7dfd2c82d5debd1264fd03d
Carlos de la Guardia <cguardia@yahoo.com>
2021-01-28 16:39:20 -0600

    pandas version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup_args = dict(
             "networkx<2.0",  # 2.0 is incompatible with odo
             "openpyxl<2.5",  # 2.5 is incompatible with tablib
             "odo@git+https://github.com/blaze/odo.git",
-            "pandas==1.2.1",
+            "pandas",
             "tablib<0.12",    # 0.12 is incompatible
         ],
         'jupyter': [
