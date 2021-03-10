80c000bffb1d7cf5ac0c95dee413a8f65ddd81db
Silvio Tomatis <silviot@gmail.com>
2021-03-10 18:13:41 +0100

    Add command to start redis and postgresql services using docker

--- dallinger/command_line/docker.py
+++ dallinger/command_line/docker.py
@@ -1,4 +1,5 @@
 import click
+import os
 
 from dallinger.command_line.utils import Output
 from dallinger.command_line.utils import header
@@ -41,3 +42,14 @@ def debug(verbose, bot, proxy, no_browsers=False, exp_config=None):
     )
     log(header, chevrons=False)
     debugger.run()
+
+
+@docker.command()
+def start_services():
+    """Starts postgresql and redis services using docker"""
+    os.system(
+        "docker run --rm -d --name dallinger_redis -p 6379:6379 -v dallinger_redis:/data redis redis-server --appendonly yes"
+    )
+    os.system(
+        "docker run -d --name dallinger_postgres -p 5432:5432 -e POSTGRES_USER=dallinger -e POSTGRES_PASSWORD=dallinger -e POSTGRES_DB=dallinger -v dallinger_postgres:/var/lib/postgresql/data postgres:12"
+    )
