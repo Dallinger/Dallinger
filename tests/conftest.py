391da7a8a6d654f7c039201a8af7e2e23811ff1e
Silvio Tomatis <silviot@gmail.com>
2021-03-18 16:33:49 +0100

    Fix test: pretend we're not installed in editable mode while testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -106,9 +106,15 @@ def experiment_dir_merged(experiment_dir, active_config):
 
     current_dir = os.getcwd()
     ensure_constraints_file_presence(current_dir)
-    destination = assemble_experiment_temp_dir(active_config)
-    os.chdir(destination)
-    yield
+    with mock.patch(
+        "dallinger.deployment.get_editable_dallinger_path"
+    ) as get_editable_dallinger_path:
+        # When dallinger is not installed as editable egg the requirements
+        # file sent to heroku will include a version pin
+        get_editable_dallinger_path.return_value = False
+        destination = assemble_experiment_temp_dir(active_config)
+        os.chdir(destination)
+        yield
     os.chdir(current_dir)
 
 
