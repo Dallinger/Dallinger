7ab6202b8a6367b7fc3d884834c9125242b346fe
Silvio Tomatis <silviot@gmail.com>
2021-05-04 17:36:30 +0200

    Refactor and connect running experiment to http server

--- dallinger/command_line/docker_ssh.py
+++ dallinger/command_line/docker_ssh.py
@@ -19,12 +19,12 @@ DOCKER_COMPOSE_EXP_TPL = Template(
 )
 
 
-CADDYFILE = b"""
+CADDYFILE = """
 # This is a configuration file for the Caddy http Server
 # Documentation can be found at https://caddyserver.com/docs
-localhost {
+{host} {{
     tls internal
-}
+}}
 
 import caddy.d/*
 """
@@ -48,29 +48,17 @@ def docker_ssh():
 @click.option("--host", required=True, help="Server name to deploy to")
 @click.option("--config", "-c", "config_options", nargs=2, multiple=True)
 def deploy(mode, image, host, config_options):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    print(f"Connecting to {host}")
-    client.connect(host)
-    print("Connected. Launching postgresql and http server")
-    client.exec_command("mkdir -p dallinger")
-    sftp = client.open_sftp()
+    executor = Executor(host)
+    executor.run("mkdir -p dallinger/caddy.d")
+
+    sftp = get_sftp(host)
     sftp.putfo(BytesIO(DOCKER_COMPOSE_SERVER), "dallinger/docker-compose.yml")
-    sftp.putfo(BytesIO(CADDYFILE), "dallinger/Caddyfile")
-    channel = client.get_transport().open_session()
-    channel.exec_command("docker-compose -f dallinger/docker-compose.yml up -d")
-    status = channel.recv_exit_status()
-    if status != 0:
-        print(f"Error: exit code was not 0 ({status})")
-        print(channel.recv(10 ** 10).decode())
-        print(channel.recv_stderr(10 ** 10).decode())
-        raise click.Abort
-    print("Launched http and postgresql servers")
+    sftp.putfo(BytesIO(CADDYFILE.format(host=host).encode()), "dallinger/Caddyfile")
+    executor.run("docker-compose -f dallinger/docker-compose.yml up -d")
+    print("Launched http and postgresql servers. Starting experiment")
 
     experiment_id = uuid4().hex[:8]
-    channel = client.get_transport().open_session()
-    channel.exec_command(f"mkdir -p dallinger/{experiment_id}")
-    status = channel.recv_exit_status()
+    executor.run(f"mkdir -p dallinger/{experiment_id}")
     sftp.putfo(
         BytesIO(
             DOCKER_COMPOSE_EXP_TPL.render(
@@ -79,21 +67,64 @@ def deploy(mode, image, host, config_options):
         ),
         f"dallinger/{experiment_id}/docker-compose.yml",
     )
-    channel = client.get_transport().open_session()
-    channel.exec_command(
+    executor.run(
         f"docker-compose -f dallinger/{experiment_id}/docker-compose.yml up -d"
     )
-    status = channel.recv_exit_status()
-    if status != 0:
-        print(
-            f"Error while starting experiment servers: exit code was not 0 ({status})"
-        )
-        print(channel.recv(10 ** 10).decode())
-        print(channel.recv_stderr(10 ** 10).decode())
-        raise click.Abort
+    print(f"Experiment {experiment_id} started")
 
-    client.close()
+    executor.run(
+        f"docker-compose -f dallinger/{experiment_id}/docker-compose.yml up -d"
+    )
+
+    caddy_conf = f"{experiment_id}.{host} {{\n    tls internal\n    reverse_proxy {experiment_id}_web:5000\n}}"
+    sftp.putfo(
+        BytesIO(caddy_conf.encode()),
+        f"dallinger/caddy.d/{experiment_id}",
+    )
+
+    # Tell caddy we changed something in the configuration
+    executor.run(
+        "docker-compose -f dallinger/docker-compose.yml exec -T httpserver caddy reload -config /etc/caddy/Caddyfile"
+    )
+
     print("To display the logs for this experiment you can run:")
     print(
         f"ssh {host} docker-compose -f dallinger/{experiment_id}/docker-compose.yml logs -f"
     )
+
+
+class Executor:
+    """Execute remote commands using paramiko"""
+
+    def __init__(self, host):
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        print(f"Connecting to {host}")
+        self.client.connect(host)
+        print("Connected.")
+
+    def run(self, cmd):
+        """Run the given command and block until it completes.
+        If the command fails, print the reason and raise an exception
+        """
+        channel = self.client.get_transport().open_session()
+        channel.exec_command(cmd)
+        status = channel.recv_exit_status()
+        print(f"Executing {cmd}")
+        if status != 0:
+            print(f"Error: exit code was not 0 ({status})")
+            print(channel.recv(10 ** 10).decode())
+            print(channel.recv_stderr(10 ** 10).decode())
+            raise ExecuteException
+        return channel.recv(10 ** 10).decode()
+
+
+class ExecuteException(Exception):
+    pass
+
+
+def get_sftp(host):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.connect(host)
+    return client.open_sftp()
