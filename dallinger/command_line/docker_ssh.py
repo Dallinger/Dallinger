07864720a5ac44dfcb598413edf99f8d4f91c812
Silvio Tomatis <silviot@gmail.com>
2021-05-04 16:42:22 +0200

    Deploy the experiment

--- dallinger/command_line/docker_ssh.py
+++ dallinger/command_line/docker_ssh.py
@@ -1,5 +1,7 @@
 from io import BytesIO
+from uuid import uuid4
 
+from jinja2 import Template
 import click
 import paramiko
 
@@ -9,6 +11,13 @@ from dallinger.utils import abspath_from_egg
 DOCKER_COMPOSE_SERVER = abspath_from_egg(
     "dallinger", "dallinger/docker/ssh_templates/docker-compose-server.yml"
 ).read_bytes()
+
+DOCKER_COMPOSE_EXP_TPL = Template(
+    abspath_from_egg(
+        "dallinger", "dallinger/docker/ssh_templates/docker-compose-experiment.yml.j2"
+    ).read_text()
+)
+
 
 CADDYFILE = b"""
 # This is a configuration file for the Caddy http Server
@@ -41,7 +50,9 @@ def docker_ssh():
 def deploy(mode, image, host, config_options):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
+    print(f"Connecting to {host}")
     client.connect(host)
+    print("Connected. Launching postgresql and http server")
     client.exec_command("mkdir -p dallinger")
     sftp = client.open_sftp()
     sftp.putfo(BytesIO(DOCKER_COMPOSE_SERVER), "dallinger/docker-compose.yml")
@@ -49,6 +60,40 @@ def deploy(mode, image, host, config_options):
     channel = client.get_transport().open_session()
     channel.exec_command("docker-compose -f dallinger/docker-compose.yml up -d")
     status = channel.recv_exit_status()
-    print(f"Status: {status}")
-    channel.close()
+    if status != 0:
+        print(f"Error: exit code was not 0 ({status})")
+        print(channel.recv(10 ** 10).decode())
+        print(channel.recv_stderr(10 ** 10).decode())
+        raise click.Abort
+    print("Launched http and postgresql servers")
+
+    experiment_id = uuid4().hex[:8]
+    channel = client.get_transport().open_session()
+    channel.exec_command(f"mkdir -p dallinger/{experiment_id}")
+    status = channel.recv_exit_status()
+    sftp.putfo(
+        BytesIO(
+            DOCKER_COMPOSE_EXP_TPL.render(
+                experiment_id=experiment_id, experiment_image=image
+            ).encode()
+        ),
+        f"dallinger/{experiment_id}/docker-compose.yml",
+    )
+    channel = client.get_transport().open_session()
+    channel.exec_command(
+        f"docker-compose -f dallinger/{experiment_id}/docker-compose.yml up -d"
+    )
+    status = channel.recv_exit_status()
+    if status != 0:
+        print(
+            f"Error while starting experiment servers: exit code was not 0 ({status})"
+        )
+        print(channel.recv(10 ** 10).decode())
+        print(channel.recv_stderr(10 ** 10).decode())
+        raise click.Abort
+
     client.close()
+    print("To display the logs for this experiment you can run:")
+    print(
+        f"ssh {host} docker-compose -f dallinger/{experiment_id}/docker-compose.yml logs -f"
+    )
