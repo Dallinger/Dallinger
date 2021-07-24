68174e5f0d581e6e42fbcbb57fed0a09e94235ef
Jesse Snyder <jesse@rasikaconsulting.com>
2021-07-24 13:17:42 -0700

    I'm done needing template search explanations

--- dallinger/dev_server/app.py
+++ dallinger/dev_server/app.py
@@ -4,8 +4,7 @@ import gevent.monkey
 
 gevent.monkey.patch_all()  # Patch before importing app and all its dependencies
 
-from dallinger.experiment_server.experiment_server import app  # noqa: E402
+from dallinger.experiment_server.experiment_server import app  # noqa: E402, F401
 
 
-app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 os.environ["FLASK_SECRET_KEY"] = codecs.encode(os.urandom(16), "hex").decode("ascii")
