730601d62ea18cfa855b014cdf0b0da074671d1d
Jesse Snyder <jesse@rasikaconsulting.com>
2021-07-27 15:02:58 -0700

    Attempt at more graceful process management

--- dallinger/dev_server/app.py
+++ dallinger/dev_server/app.py
@@ -1,9 +1,9 @@
-import codecs
-import os
 import gevent.monkey
 
 gevent.monkey.patch_all()  # Patch before importing app and all its dependencies
 
+import codecs  # noqa: E402
+import os  # noqa: E402
 from dallinger.experiment_server.experiment_server import app  # noqa: E402, F401
 
 
