20d14a049db40df3e6035b8f031260ae7bc72700
Silvio Tomatis <silviot@gmail.com>
2021-09-24 00:31:28 +0200

    Include hash of requirements.txt in constraints.txt to prove we did our homework

--- scripts/update_experiments_constraints.sh
+++ scripts/update_experiments_constraints.sh
@@ -11,6 +11,7 @@ for demo_name in $(ls demos/dlgr/demos/); do
 -r requirements.txt" > temp-requirements.txt
         pip-compile temp-requirements.txt -o constraints.txt
         rm temp-requirements.txt
+        echo '# generate from file with hash ' $(md5sum requirements.txt) >> constraints.txt
         cd -
     fi
 done
