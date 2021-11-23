90cad7f412694e511c2da3f2ccc644005bf2d956
Frank Höger <frank.hoeger@ae.mpg.de>
2021-11-23 17:46:56 +0100

    Add sndfile buildpack

--- dallinger/deployment.py
+++ dallinger/deployment.py
@@ -134,7 +134,7 @@ def deploy_sandbox_shared_setup(
     heroku_app = HerokuApp(dallinger_uid=heroku_app_id, output=out, team=team)
     heroku_app.bootstrap()
     heroku_app.buildpack("https://github.com/stomita/heroku-buildpack-phantomjs")
-    heroku_app.buildpack("https://github.com/heroku/heroku-buildpack-apt")
+    heroku_app.buildpack("https://github.com/primaryobjects/heroku-buildpack-libsndfile")
 
     # Set up add-ons and AWS environment variables.
     database_size = config.get("database_size")
