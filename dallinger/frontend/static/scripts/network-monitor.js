2e3b48abc974f58bb92fd0e53d68f093161da56a
Peter Harrison <pmch2@cam.ac.uk>
2022-03-21 01:14:14 +0000

    Fix visualization for networks with no nodes

--- dallinger/frontend/static/scripts/network-monitor.js
+++ dallinger/frontend/static/scripts/network-monitor.js
@@ -433,11 +433,6 @@
         }
       }
 
-      if (!is_found) {
-        to=to_min; // use a default strategy that the source is the minimal id
-        //to=my_node_id
-      }
-
       if (net.property1=='True') { // assume that there is a feature in property1 that decide if a network is open or close
         mgroup='group_networks_open';
       } else {
@@ -460,11 +455,13 @@
         data: net
 
       });
-      edges.push({
-        from: from,
-        to: to,
-        color: 'black'
-      });
+      if (is_found) {
+        edges.push({
+          from: from,
+          to: to,
+          color: 'black'
+        });
+      }
       //connect network to father
       edges.push({
         from: father,
