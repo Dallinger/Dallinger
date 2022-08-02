797aa33ad200a698d9af1eba1cb95e0100de4eb9
Peter Harrison <pmch2@cam.ac.uk>
2022-08-02 10:24:04 +0100

    Catch AlreadyFailedError in failure cascade

--- dallinger/models.py
+++ dallinger/models.py
@@ -161,7 +161,9 @@ class SharedMixin(object):
         the `failure_cascade` property.
         """
         if self.failed is True:
-            raise AttributeError("Cannot fail {} - it has already failed.".format(self))
+            raise AlreadyFailedError(
+                "Cannot fail {} - it has already failed.".format(self)
+            )
         else:
             self.failed = True
             self.failed_reason = reason
@@ -175,6 +177,14 @@ class SharedMixin(object):
                     obj.fail(reason=wrapped_reason)
                 except TypeError:
                     obj.fail()
+                except AlreadyFailedError:
+                    pass
+
+
+class AlreadyFailedError(AttributeError):
+    """
+    Raised when trying to fail an object that has already been failed.
+    """
 
 
 class Participant(Base, SharedMixin):
