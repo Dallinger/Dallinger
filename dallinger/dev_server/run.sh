bdc3ecc63590d0d6003590edb9464f0489d20e0a
Peter Harrison <pmch2@cam.ac.uk>
2022-09-07 16:13:14 +0100

    Update for latest Flask version (#4338)
    - Remove the not-supported eager-loading option from the flask run command
    - Replace the deprecated FLASK_ENV command with FLASK_DEBUG

--- dallinger/dev_server/run.sh
+++ dallinger/dev_server/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export FLASK_APP="app.py"
-export FLASK_ENV="development"
+export FLASK_DEBUG=1
 
 # --eager-loading is required because we're patching IO with gevent.monkey.patch_all()
 # See somewhat indirect reference:
 # https://github.com/miguelgrinberg/Flask-SocketIO/issues/901
 echo "Starting flask..."
 echo "Remember to terminate this process with <control-c> before running 'dallinger bootstrap' again"
-flask run --eager-loading "$@"
+flask run "$@"
