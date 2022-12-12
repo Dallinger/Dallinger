9303c6010165b01849d12f50d325092c36d8b504
Frank Höger <frank.hoeger@ae.mpg.de>
2022-12-12 19:45:26 +0100

    Update dependencies; set jupyter-server==1.23.3

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup_args = dict(
     extras_require={
         "jupyter": [
             "jupyter",
+            "jupyter-server==1.23.3",
             "ipywidgets",
         ],
         "data": [
