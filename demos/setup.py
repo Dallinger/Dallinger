3084e0aff40b874ed2928012b9f4f33706d9bfc4
Frank Höger <frank.hoeger@ae.mpg.de>
2023-04-19 00:00:32 +0200

    Bump version on master branch post-release

--- demos/setup.py
+++ demos/setup.py
@@ -10,7 +10,7 @@ if sys.argv[-1] == "publish":
 
 setup_args = dict(
     name="dlgr.demos",
-    version="9.6.0",
+    version="9.7.0a1",
     description="Demonstration experiments for Dallinger",
     url="http://github.com/Dallinger/Dallinger",
     maintainer="Jordan Suchow",
