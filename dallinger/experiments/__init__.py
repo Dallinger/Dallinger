27e0e5f8192f7a8387dc311e1c06ceedaebae3fe
Frank Höger <frank.hoeger@ae.mpg.de>
2023-05-11 21:58:53 +0200

    Migrate 'pkg_resources import iter_entry_points' to 'importlib.metadata import entry_points'

--- dallinger/experiments/__init__.py
+++ dallinger/experiments/__init__.py
@@ -2,8 +2,7 @@
 with a ``setuptools`` ``entry_point`` for the ``dallinger.experiments`` group.
 """
 import logging
-
-from pkg_resources import iter_entry_points
+from importlib.metadata import entry_points
 
 from ..experiment import Experiment
 
@@ -13,7 +12,7 @@ logger.addHandler(logging.NullHandler())
 # Avoid PEP-8 warning, we want this to be importable from this location
 Experiment = Experiment
 
-for entry_point in iter_entry_points(group="dallinger.experiments"):
+for entry_point in entry_points().get("dallinger.experiments"):
     try:
         globals()[entry_point.name] = entry_point.load()
     except ImportError:
