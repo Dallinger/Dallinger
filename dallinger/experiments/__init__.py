c6460e3e57b8d549e5a5232a1d64ad2a49968577
Frank Höger <frank.hoeger@ae.mpg.de>
2023-05-11 23:29:33 +0200

    Try fixing getting entry_points

--- dallinger/experiments/__init__.py
+++ dallinger/experiments/__init__.py
@@ -2,9 +2,8 @@
 with a ``setuptools`` ``entry_point`` for the ``dallinger.experiments`` group.
 """
 import logging
-from importlib.metadata import entry_points
 
-from dallinger import experiments  # noqa: F401
+from dallinger import experiments
 
 from ..experiment import Experiment
 
@@ -14,7 +13,7 @@ logger.addHandler(logging.NullHandler())
 # Avoid PEP-8 warning, we want this to be importable from this location
 Experiment = Experiment
 
-for entry_point in entry_points().get("dallinger.experiments"):
+for entry_point in experiments.entry_points().get("dallinger.experiments"):
     try:
         globals()[entry_point.name] = entry_point.load()
     except ImportError:
