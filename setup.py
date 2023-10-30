e2845fac9cec8865a0e54073e892ffc92344dbb7
Frank Höger <frank.hoeger@ae.mpg.de>
2023-10-30 23:38:04 +0100

    Revert "Remove pinning of ipython"
    This reverts commit 76774b5d479b4fa2844b0d4a71a5b5ef6e853fe0.

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup_args = dict(
         "greenlet",
         "gunicorn[gevent]",
         "heroku3",
+        "ipython < 8.13",
         "localconfig",
         "numpy < 1.25",
         "pandas < 2.1",
