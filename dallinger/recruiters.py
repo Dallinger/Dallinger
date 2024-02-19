2c2dbd13e9401e9979e3bcc92e0c02c7d85ff749
Jesse Snyder <jesse@rasikaconsulting.com>
2024-02-19 12:08:23 -0800

    Address issues from PR review

--- dallinger/recruiters.py
+++ dallinger/recruiters.py
@@ -1354,7 +1354,7 @@ class MTurkRecruiter(Recruiter):
                 )
                 if participant is None:
                     logger.error(
-                        "Recieved an AssignmentSubmitted notification from MTurk for assignment ID {}, "
+                        "Received an AssignmentSubmitted notification from MTurk for assignment ID {}, "
                         "which is not related to any participant.".format(assignment_id)
                     )
                     return
@@ -1432,7 +1432,7 @@ class MTurkRecruiter(Recruiter):
                 try:
                     self.mturkservice.get_qualification_type_by_name(new["name"])
                 except QualificationNotFoundException:
-                    logger.warn(
+                    logger.warning(
                         "Did not find qualification {}. Trying again...".format(
                             new["name"]
                         )
@@ -1445,7 +1445,7 @@ class MTurkRecruiter(Recruiter):
 
         unavailable = [q for q in result["new_qualifications"] if not q["available"]]
         if unavailable:
-            logger.warn(
+            logger.warning(
                 "After several attempts, some qualifications are still not ready "
                 "for assignment: {}".format(", ".join(unavailable))
             )
