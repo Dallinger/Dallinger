279a0f9553e945c71b65cfa34d6647f493d94a55
Jesse Snyder <jesse@rasikaconsulting.com>
2024-02-19 14:16:59 -0800

    Lock only the participant DB row, not related records in other tables

--- dallinger/experiment_server/experiment_server.py
+++ dallinger/experiment_server/experiment_server.py
@@ -1738,7 +1738,7 @@ def _worker_complete(participant_id):
     # double-submits:
     participant = (
         models.Participant.query.populate_existing()
-        .with_for_update()
+        .with_for_update(of=models.Participant)
         .get(participant_id)
     )
     if participant is None:
