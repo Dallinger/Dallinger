8630c048e8d77d0714fdfac44131ca1f91191b0d
fmhoeger <fmhoeger@mirsem.org>
2024-12-30 19:50:01 +0100

    Upgrade to Python 3.13; remove Python 3.9

--- dallinger/docker/tools.py
+++ dallinger/docker/tools.py
@@ -76,6 +76,9 @@ class DockerComposeWrapper(object):
         if editable_dallinger_path:
             volumes.append(f"{editable_dallinger_path}/dallinger:/dallinger/dallinger")
             volumes.append(
+                f"{editable_dallinger_path}/dallinger:/usr/local/lib/python3.13/dist-packages/dallinger/"
+            )
+            volumes.append(
                 f"{editable_dallinger_path}/dallinger:/usr/local/lib/python3.12/dist-packages/dallinger/"
             )
             volumes.append(
@@ -83,9 +86,6 @@ class DockerComposeWrapper(object):
             )
             volumes.append(
                 f"{editable_dallinger_path}/dallinger:/usr/local/lib/python3.10/dist-packages/dallinger/"
-            )
-            volumes.append(
-                f"{editable_dallinger_path}/dallinger:/usr/local/lib/python3.9/dist-packages/dallinger/"
             )
         tag = get_experiment_image_tag(self.tmp_dir)
         with open(os.path.join(self.tmp_dir, "docker-compose.yml"), "w") as fh:
