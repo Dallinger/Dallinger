cb73e1066a431e7c52da46357e2ad15b60f63ac0
fmhoeger <fmhoeger@mirsem.org>
2025-03-24 15:43:30 +0100

    Work on screen_out_allowed

--- dallinger/utils.py
+++ dallinger/utils.py
@@ -28,6 +28,7 @@ from pythonjsonlogger import jsonlogger
 from dallinger import db
 from dallinger.compat import is_command
 from dallinger.config import get_config
+from dallinger.models import Participant
 from dallinger.version import __version__
 
 try:
@@ -996,3 +997,23 @@ def classproperty(func):
         func = classmethod(func)
 
     return ClassPropertyDescriptor(func)
+
+
+def median_time_spent(participants: list[Participant]):
+    if not participants:
+        return 0
+
+    times = [
+        participant.end_time - participant.creation_time
+        for participant in participants
+        if participant.end_time is not None
+    ]
+    times.sort()
+
+    mid = len(times) // 2
+    if len(times) % 2 == 0:
+        median_time = (times[mid - 1] + times[mid]) / 2
+    else:
+        median_time = times[mid]
+
+    return median_time
