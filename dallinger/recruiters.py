6a936f739bb4798270ec4793db670479b8b8ff83
fmhoeger <fmhoeger@mirsem.org>
2025-04-02 19:53:06 +0200

    Remove screen_out_allowed condition in ProlificRecruiter.screen_out

--- dallinger/recruiters.py
+++ dallinger/recruiters.py
@@ -860,16 +860,15 @@ class ProlificRecruiter(Recruiter):
             participant: Participant
                 The participant to screen-out.
         """
-        if self.screen_out_allowed(participant):
-            try:
-                return self.prolificservice.screen_out(
-                    study_id=self.current_study_id,
-                    submission_id=participant.assignment_id,
-                    bonus_per_submission=participant.base_pay + participant.bonus,
-                    increase_places=self.config.get("auto_recruit"),
-                )
-            except ProlificServiceException as ex:
-                logger.exception(str(ex))
+        try:
+            return self.prolificservice.screen_out(
+                study_id=self.current_study_id,
+                submission_id=participant.assignment_id,
+                bonus_per_submission=participant.base_pay + participant.bonus,
+                increase_places=self.config.get("auto_recruit"),
+            )
+        except ProlificServiceException as ex:
+            logger.exception(str(ex))
 
     def screen_out_allowed(self, participant):
         """Check if a participant can be screened out.
