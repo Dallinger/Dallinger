c987b986eaf4621dd0c1f021b0ff0f628bf9fc13
fmhoeger <fmhoeger@mirsem.org>
2025-06-06 21:10:04 +0200

    Refactor

--- dallinger/command_line/docker_ssh.py
+++ dallinger/command_line/docker_ssh.py
@@ -804,23 +804,24 @@ def get_connected_ssh_client(host, user=None) -> paramiko.SSHClient:
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.load_system_host_keys()
 
-    print(f"Connecting to {host}...")
-    config = get_config()
-    config.load()
-    server_pem = config.get("server_pem", None)
-    if server_pem:
-        if not os.path.exists(server_pem):
-            raise FileNotFoundError(
-                f"SSH key file not found: {server_pem}\n"
-                "Please check that the path in your config file is correct.\n"
-                "You can set the path in either:\n"
-                "  - Your experiment's config.txt: server_pem = /path/to/key.pem\n"
-                "  - Your global config ~/.dallingerconfig: server_pem = /path/to/key.pem"
-            )
-        client.connect(host, username=user, key_filename=server_pem)
-    else:
-        client.connect(host, username=user)
-    print("Connected.")
+    print(f"Connecting to {host}")
+    with yaspin() as spinner:
+        config = get_config()
+        config.load()
+        server_pem = config.get("server_pem", None)
+        if server_pem:
+            if not os.path.exists(server_pem):
+                raise FileNotFoundError(
+                    f"SSH key file not found: {server_pem}\n"
+                    "Please check that the path in your config file is correct.\n"
+                    "You can set the path in either:\n"
+                    "  - Your experiment's config.txt: server_pem = /path/to/key.pem\n"
+                    "  - Your global config ~/.dallingerconfig: server_pem = /path/to/key.pem"
+                )
+            client.connect(host, username=user, key_filename=server_pem)
+        else:
+            client.connect(host, username=user)
+        spinner.ok("Connected.")
 
     return client
 
