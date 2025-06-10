10e3ccf77052be3354185a8ec670a3a71fcfd4a6
fmhoeger <fmhoeger@mirsem.org>
2025-06-10 15:57:28 +0200

    Consider Papertrail logs in Heroku deployment

--- dallinger/deployment.py
+++ dallinger/deployment.py
@@ -98,7 +98,15 @@ def handle_launch_data(
     error("Experiment launch failed after multiple attempts.")
     if launch_data and launch_data.get("message"):
         error(launch_data["message"])
-    if dns_host and dozzle_password:
+
+    # Check if this is a Heroku deployment by looking at the host
+    parsed_url = urlparse(url)
+    if parsed_url.netloc.endswith("herokuapp.com"):
+        print_bold(
+            "For detailed server logs, visit the Papertrail add-on in your Heroku dashboard"
+        )
+    # If this is a Docker deployment, we can use Dozzle to view the logs
+    elif dns_host and dozzle_password:
         print_bold(
             f"Check the detailed server logs at https://logs.{dns_host} (user = dallinger, password = {dozzle_password})"
         )
