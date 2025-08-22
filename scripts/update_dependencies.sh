26bd26566622afac970650e4e7563aa0b4f955c8
fmhoeger <fmhoeger@mirsem.org>
2025-08-22 22:34:45 +0200

    Minor fix in update_dependencies.sh

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -9,12 +9,10 @@ pip-compile --rebuild --strip-extras --upgrade dev-requirements.in
 pip-compile --rebuild --strip-extras --upgrade
 
 # Remove the line specifying dallinger as editable dependency
-sed -e "s/^-e.*//" -i.bak *.txt
-
-# Post-process the constraints.txt and dev-requirements.txt files to replace "via file://..."
-# with "via constraints.in" and " via dev-requirements.in", respectively.
-sed -i.bak 's|via file://.*|via -r constraints.in|' constraints.txt
-sed -i.bak 's|via file://.*|via -r dev-requirements.in|' dev-requirements.txt
+sed -e "/^-e/d" -i.bak *.txt
+# Fix "via -" annotations
+sed -i.bak 's|^    # via -$|    # via -r constraints.in|' constraints.txt
+sed -i.bak 's|^    # via -$|    # via -r dev-requirements.in|' dev-requirements.txt
 
 # Cleanup
 rm constraints.txt.bak dev-requirements.txt.bak
